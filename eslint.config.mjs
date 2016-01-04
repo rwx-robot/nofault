05b097a913144cf04bac8edbf8ed34b92f3b9132
nofault maintainers <maintainers@nofault.io>
2016-01-04 23:19:20 +0000

    chore(monorepo): refine pnpm workspace layout

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,3 +30,11 @@ export default tseslint.config(
     files: ['scripts/**/*.mjs', 'benchmarks/**/*.mjs'],
     rules: { 'no-console': 'off' },
   },
+  {
+    // CLI 入口是 CJS 薄壳（`bin` 必须在任何打包器之外可执行），require 是唯一选择
+    files: ['packages/*/bin/**/*.js'],
+    rules: { '@typescript-eslint/no-require-imports': 'off' },
+  },
+  {
+    // 示例与测试放宽限制
+    files: ['examples/**/*.ts', 'tests/**/*.ts', '**/*.test.ts'],
