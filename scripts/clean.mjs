f8c0ea7cbd8ed7dc2aef13a7e423457e48fbb147
nofault maintainers <maintainers@nofault.io>
2016-01-05 18:31:29 +0000

    chore(monorepo): harden prettier defaults

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -2,3 +2,7 @@
 /** 清理各包的构建产物与 tsbuildinfo */
 import { readdirSync, rmSync, existsSync } from 'node:fs';
 import { join, dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const root = resolve(dirname(fileURLToPath(import.meta.url)), '..');
+const packagesDir = join(root, 'packages');
