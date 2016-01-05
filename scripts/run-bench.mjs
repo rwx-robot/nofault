732a7cca6c7f6fb60c594a290d890de70fc13bf4
Ny <johnnynode@gmail.com>
2016-01-05 22:49:33 +0000

    chore(monorepo): test pnpm workspace layout

--- scripts/run-bench.mjs
+++ scripts/run-bench.mjs
@@ -6,31 +6,3 @@
  *   pnpm bench v0.1.0
  *   node scripts/run-bench.mjs v0.1.0 --duration=10 --connections=64
  */
-import { spawnSync } from 'node:child_process';
-import { existsSync, readdirSync } from 'node:fs';
-import { join, dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const root = resolve(dirname(fileURLToPath(import.meta.url)), '..');
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  const available = existsSync(join(root, 'benchmarks'))
-    ? readdirSync(join(root, 'benchmarks'), { withFileTypes: true })
-        .filter((d) => d.isDirectory())
-        .map((d) => d.name)
-    : [];
-  console.log(`用法: node scripts/run-bench.mjs <tag> [args...]\n可用:\n${available.map((n) => `  - ${n}`).join('\n')}`);
-  process.exit(available.length ? 0 : 1);
-}
-
-const tag = args[0];
-const benchDir = join(root, 'benchmarks', tag);
-const script = existsSync(join(benchDir, 'bench.mjs')) ? join(benchDir, 'bench.mjs') : join(benchDir, 'bench.js');
-if (!existsSync(script)) {
-  console.error(`[bench] 未找到脚本: ${script}`);
-  process.exit(1);
-}
-
-const r = spawnSync(process.execPath, [script, ...args.slice(1)], { cwd: benchDir, stdio: 'inherit' });
-process.exit(r.status ?? 1);
