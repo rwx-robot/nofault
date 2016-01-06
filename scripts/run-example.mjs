58744497b787f13ec73087654d39cab5d6a9f1f0
nofault maintainers <maintainers@nofault.io>
2016-01-06 10:10:58 +0000

    chore(monorepo): test eslint flat config

--- scripts/run-example.mjs
+++ scripts/run-example.mjs
@@ -12,3 +12,16 @@
 import { spawnSync, spawn } from 'node:child_process';
 import { existsSync, readdirSync } from 'node:fs';
 import { join, dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const root = resolve(dirname(fileURLToPath(import.meta.url)), '..');
+const args = process.argv.slice(2);
+
+if (args.length === 0 || args[0] === '--help' || args[0] === '-h') {
+  const available = existsSync(join(root, 'examples'))
+    ? readdirSync(join(root, 'examples'), { withFileTypes: true })
+        .filter((d) => d.isDirectory())
+        .map((d) => d.name)
+    : [];
+  console.log(`用法: node scripts/run-example.mjs <example-dir> [ENV=value ...]\n`);
+  console.log(`可用示例:\n${available.map((n) => `  - ${n}`).join('\n')}`);
