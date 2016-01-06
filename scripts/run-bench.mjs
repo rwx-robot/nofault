567e70456acf1897276a6008734ac91b1946308c
Ny <johnnynode@gmail.com>
2016-01-06 10:59:26 +0000

    chore(monorepo): test prettier defaults

--- scripts/run-bench.mjs
+++ scripts/run-bench.mjs
@@ -28,3 +28,9 @@ const tag = args[0];
 const benchDir = join(root, 'benchmarks', tag);
 const script = existsSync(join(benchDir, 'bench.mjs')) ? join(benchDir, 'bench.mjs') : join(benchDir, 'bench.js');
 if (!existsSync(script)) {
+  console.error(`[bench] 未找到脚本: ${script}`);
+  process.exit(1);
+}
+
+const r = spawnSync(process.execPath, [script, ...args.slice(1)], { cwd: benchDir, stdio: 'inherit' });
+process.exit(r.status ?? 1);
