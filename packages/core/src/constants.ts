78a6fc789580791640bc8174a813891725dfeecd
nofault maintainers <maintainers@nofault.io>
2016-02-15 19:36:42 +0000

    feat(core): implement container registry

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -16,3 +16,6 @@ export const PROVIDER_METADATA = {
 export const PARAM_TYPES_METADATA = 'design:paramtypes';
 export const PROPERTY_TYPE_METADATA = 'design:type';
 export const RETURN_TYPE_METADATA = 'design:returntype';
+
+/** 内置令牌 */
+export const APP_OPTIONS = Symbol('NOFAULT_APP_OPTIONS');
