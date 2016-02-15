42f1ed73a350b983f1f9ee6579131aca7e66148f
nofault maintainers <maintainers@nofault.io>
2016-02-15 23:06:57 +0000

    feat(core): implement value providers

--- packages/core/src/container/container.ts
+++ packages/core/src/container/container.ts
@@ -107,3 +107,31 @@ export class NofaultContainer {
    *
    * 为什么必须做：单例缓存住一个请求级对象，会导致**跨请求数据串号**，
    * 这类 bug 在压测和线上偶发，靠 code review 很难发现。
+   */
+  propagateRequestScope(): void {
+    const byToken = new Map<InjectionToken, InstanceWrapper>();
+    for (const mod of this.modules.values()) {
+      for (const [token, wrapper] of mod.providers) byToken.set(token, wrapper);
+    }
+
+    const depsOf = (wrapper: InstanceWrapper): InjectionToken[] => {
+      const def = this.findProviderDef(wrapper.token);
+      if (!def || typeof def === 'function') return readParamTypes(def ?? (wrapper.token as never));
+      if (isClassProvider(def)) return readParamTypes(def.useClass);
+      if (isFactoryProvider(def)) return def.inject ?? [];
+      if (isExistingProvider(def)) return [def.useExisting];
+      return [];
+    };
+
+    let changed = true;
+    let guard = 0;
+    while (changed && guard++ < 100) {
+      changed = false;
+      for (const mod of this.modules.values()) {
+        for (const wrapper of mod.providers.values()) {
+          if (wrapper.scope === Scope.REQUEST || wrapper.contextDependent) continue;
+          for (const dep of depsOf(wrapper)) {
+            const target = byToken.get(dep);
+            if (target && (target.scope === Scope.REQUEST || target.contextDependent)) {
+              wrapper.contextDependent = true;
+              changed = true;
