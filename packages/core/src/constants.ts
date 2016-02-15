39eec37dec3f1207bdc724fae1387cf43e6d0703
nofault maintainers <maintainers@nofault.io>
2016-02-15 15:19:03 +0000

    feat(core): add property injection

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,3 +1,6 @@
 /** 框架内部使用的元数据键（统一收敛，避免散落魔法字符串） */
 
 export const MODULE_METADATA = {
+  IMPORTS: 'nofault:module:imports',
+  PROVIDERS: 'nofault:module:providers',
+  EXPORTS: 'nofault:module:exports',
