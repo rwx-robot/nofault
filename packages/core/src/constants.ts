d5a078b3634faa19e19dcd7622599b6edf552c81
Ny <johnnynode@gmail.com>
2016-02-16 16:43:21 +0000

    feat(core): refine module scanner

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -15,3 +15,4 @@ export const PROVIDER_METADATA = {
 
 export const PARAM_TYPES_METADATA = 'design:paramtypes';
 export const PROPERTY_TYPE_METADATA = 'design:type';
+export const RETURN_TYPE_METADATA = 'design:returntype';
