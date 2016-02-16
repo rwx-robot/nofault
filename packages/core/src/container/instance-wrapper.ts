ca0c7f818d20f45f62d1d5e89003484218ddae9b
nofault maintainers <maintainers@nofault.io>
2016-02-16 18:24:22 +0000

    feat(core): refine instance wrapper scopes

--- packages/core/src/container/instance-wrapper.ts
+++ packages/core/src/container/instance-wrapper.ts
@@ -31,3 +31,20 @@ export class InstanceWrapper<T = unknown> {
   public hostModule?: string;
   /** 解析栈标记：true 表示正在解析中，用于循环依赖检测 */
   public isResolving = false;
+  /**
+   * 是否"被 REQUEST 作用域污染"（captive dependency）。
+   *
+   * 典型场景：单例 Controller 依赖了 REQUEST 作用域的 Service。
+   * 若不标记，这个单例会被缓存下来，于是**所有请求共用第一个请求的对象**——
+   * 数据串号，且极难排查。标记后按上下文缓存，语义才正确。
+   */
+  public contextDependent = false;
+
+  constructor(
+    public readonly token: InjectionToken<T>,
+    public readonly scope: Scope,
+    /**
+     * 创建实例的工厂。
+     *
+     * 必须接收 contextId：Provider 的依赖里可能有 REQUEST 作用域的成员，
+     * 若工厂闭包把 contextId 固定成 undefined，请求上下文就传不下去了。
