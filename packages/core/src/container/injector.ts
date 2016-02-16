cdb5c086dc674a39e4d0d4d7b32b1e2a5f9e2023
nofault maintainers <maintainers@nofault.io>
2016-02-16 11:56:47 +0000

    feat(core): implement optional dependencies

--- packages/core/src/container/injector.ts
+++ packages/core/src/container/injector.ts
@@ -20,3 +20,25 @@ import { InstanceWrapper } from './instance-wrapper';
 import type { ContextId } from './instance-wrapper';
 import type { ModuleRef } from './module-ref';
 import type { NofaultContainer } from './container';
+
+/**
+ * 注入器：负责把一个 Provider 定义变成真实实例。
+ *
+ * 解析顺序（与 NestJS 一致）：
+ * 1. 本模块声明的 Provider
+ * 2. 导入模块的 exports
+ * 3. 全局模块
+ * 4. 抛 UnknownDependencyError
+ *
+ * `contextId` 会一路透传给依赖链，
+ * 这样 REQUEST 作用域的 Provider 才能在同一请求内共享同一个实例。
+ */
+export class Injector {
+  /** 当前解析栈，用于循环依赖检测 */
+  private readonly resolutionStack: string[] = [];
+
+  constructor(private readonly container: NofaultContainer) {}
+
+  /** 计算某个 Provider 的作用域 */
+  static resolveScope(provider: Provider): Scope {
+    if (typeof provider === 'function') return readScope(provider);
