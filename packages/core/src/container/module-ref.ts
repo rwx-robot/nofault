37dd6d8210857b2c8c41ebf9341104d5dc914330
nofault maintainers <maintainers@nofault.io>
2016-02-16 22:36:44 +0000

    feat(core): refine circular dependency detection

--- packages/core/src/container/module-ref.ts
+++ packages/core/src/container/module-ref.ts
@@ -5,3 +5,9 @@ import { tokenToString } from '../interfaces/type.interface';
 import type { InstanceWrapper } from './instance-wrapper';
 
 /**
+ * 模块容器：对应一个 `@Module()` 类。
+ *
+ * 保存该模块声明的 Provider、导入的模块、导出的令牌。
+ */
+export class ModuleRef {
+  public readonly providers = new Map<InjectionToken, InstanceWrapper>();
