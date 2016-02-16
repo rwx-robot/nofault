ee18abde8538e5ab9a18ef835c89c18bc35c3770
Ny <johnnynode@gmail.com>
2016-02-16 17:27:03 +0000

    feat(core): refine injector resolution

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -16,3 +16,5 @@ export const PROVIDER_METADATA = {
 export const PARAM_TYPES_METADATA = 'design:paramtypes';
 export const PROPERTY_TYPE_METADATA = 'design:type';
 export const RETURN_TYPE_METADATA = 'design:returntype';
+
+/** 内置令牌 */
