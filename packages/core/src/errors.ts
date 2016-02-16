604178e5204548a8c64dedd5c4cc85d8e341cbf2
Ny <johnnynode@gmail.com>
2016-02-16 21:20:48 +0000

    feat(core): refine property injection

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -5,3 +5,7 @@ import { tokenToString } from './interfaces/type.interface';
 export class UnknownDependencyError extends Error {
   constructor(token: InjectionToken, context?: string) {
     const where = context ? ` in ${context}` : '';
+    super(
+      `nofault can't resolve dependencies of the provider \`${tokenToString(token)}\`${where}. ` +
+        `Please make sure the provider is declared in a module, or the module exporting it is imported.`,
+    );
