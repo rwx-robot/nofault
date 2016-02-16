6cbff3e15c3faa96db390c51bdfbfd78da97ceb5
Ny <johnnynode@gmail.com>
2016-02-16 20:35:18 +0000

    feat(core): refine existing providers

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -2,3 +2,6 @@ import type { InjectionToken } from './interfaces/type.interface';
 import { tokenToString } from './interfaces/type.interface';
 
 /** 依赖解析失败 */
+export class UnknownDependencyError extends Error {
+  constructor(token: InjectionToken, context?: string) {
+    const where = context ? ` in ${context}` : '';
