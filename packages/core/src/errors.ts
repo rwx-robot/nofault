c82d178cca1904196a0b0643488fd5bb41117e4d
Ny <johnnynode@gmail.com>
2016-02-17 16:08:43 +0000

    feat(core): harden existing providers

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -50,3 +50,7 @@ export class InvalidProviderError extends Error {
     super(`Invalid provider definition for \`${tokenToString(token)}\`.`);
     this.name = 'InvalidProviderError';
   }
+}
+
+/** 应用已启动后仍尝试注册 */
+export class ContainerLockedError extends Error {
