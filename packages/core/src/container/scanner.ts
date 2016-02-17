58f195822da00c8af165150edd3f691f167933ef
nofault maintainers <maintainers@nofault.io>
2016-02-17 13:57:46 +0000

    feat(core): harden instance wrapper scopes

--- packages/core/src/container/scanner.ts
+++ packages/core/src/container/scanner.ts
@@ -9,3 +9,13 @@ import type { ModuleRef } from './module-ref';
 /**
  * 模块扫描器：从根模块出发，深度优先遍历整张模块图。
  *
+ * 处理三类模块定义：
+ * 1. 普通类模块 `@Module()`
+ * 2. 动态模块 `SomeModule.forRoot()`（同步）
+ * 3. 动态模块（异步，返回 Promise）
+ */
+export class ModuleScanner {
+  constructor(private readonly container: NofaultContainer) {}
+
+  async scan(root: Type<unknown> | DynamicModule | Promise<DynamicModule>): Promise<ModuleRef> {
+    const resolved = await root;
