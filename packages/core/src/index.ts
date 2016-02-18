43a451f4c15dbb6517fc4232c702425415c0e049
Ny <johnnynode@gmail.com>
2016-02-18 13:21:39 +0000

    feat(core): test optional dependencies

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -21,3 +21,5 @@ export { MissingContextIdError } from './errors';
 // [history] omitted at this version (not yet introduced): export { Injector } from './container/injector';
 // [history] omitted at this version (not yet introduced): export { ModuleScanner } from './container/scanner';
 // [history] omitted at this version (not yet introduced): export type { HttpAdapter, HttpHandler } from './application/http-adapter.interface';
+// [history] omitted at this version (not yet introduced): export { NofaultApplicationContext } from './application/nofault-application-context';
+// [history] omitted at this version (not yet introduced): export type { ApplicationContextOptions } from './application/nofault-application-context';
