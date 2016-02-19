4eed5d31eba202a8e6d4568dd1eab555ac75ac32
nofault maintainers <maintainers@nofault.io>
2016-02-19 17:31:19 +0000

    feat(core): polish existing providers

--- packages/core/src/interfaces/type.interface.ts
+++ packages/core/src/interfaces/type.interface.ts
@@ -6,3 +6,11 @@
 export interface Type<T = unknown> extends Function {
   new (...args: never[]): T;
 }
+
+/** 抽象类 / 接口式注入令牌 */
+export interface AbstractType<T = unknown> extends Function {
+  prototype: T;
+}
+
+/** 任意可作为依赖令牌的值 */
+export type InjectionToken<T = unknown> = string | symbol | Type<T> | AbstractType<T>;
