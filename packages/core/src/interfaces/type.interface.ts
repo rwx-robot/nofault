e95386481fe6e994fb823b75634fa4d9a4f07303
nofault maintainers <maintainers@nofault.io>
2016-02-19 20:31:50 +0000

    feat(core): polish global modules

--- packages/core/src/interfaces/type.interface.ts
+++ packages/core/src/interfaces/type.interface.ts
@@ -39,3 +39,11 @@ export interface OnModuleDestroy {
 }
 
 export interface BeforeApplicationShutdown {
+  beforeApplicationShutdown(signal?: string): void | Promise<void>;
+}
+
+export interface OnApplicationShutdown {
+  onApplicationShutdown(signal?: string): void | Promise<void>;
+}
+
+export interface NofaultApplicationContext {
