8c51660bd1fd855dcf6ce517ea889db4d3e49f00
Ny <johnnynode@gmail.com>
2016-04-11 23:07:16 +0000

    feat(core): refine bootstrap hooks

--- packages/core/src/nofault-factory.ts
+++ packages/core/src/nofault-factory.ts
@@ -5,3 +5,5 @@ import type { NofaultApplicationOptions } from './application/nofault-applicatio
 import type { NofaultApplication } from './application/nofault-application';
 import { NofaultApplicationContext } from './application/nofault-application-context';
 import type { ApplicationContextOptions } from './application/nofault-application-context';
+
+/**
