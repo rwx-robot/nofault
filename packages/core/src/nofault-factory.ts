70b1f0250ee485301394af26db6d3ad7c94381e0
Ny <johnnynode@gmail.com>
2016-04-11 18:14:12 +0000

    feat(core): implement shutdown hooks

--- packages/core/src/nofault-factory.ts
+++ packages/core/src/nofault-factory.ts
@@ -3,3 +3,4 @@ import type { Type } from './interfaces/type.interface';
 import { createApplication } from './application/nofault-application';
 import type { NofaultApplicationOptions } from './application/nofault-application';
 import type { NofaultApplication } from './application/nofault-application';
+import { NofaultApplicationContext } from './application/nofault-application-context';
