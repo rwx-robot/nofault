c990e321f928e7da2fe62ab74260283d521e4fcb
Ny <johnnynode@gmail.com>
2016-04-11 16:08:13 +0000

    feat(core): implement bootstrap sequence

--- packages/core/src/nofault-factory.ts
+++ packages/core/src/nofault-factory.ts
@@ -2,3 +2,4 @@ import type { DynamicModule } from './interfaces/module.interface';
 import type { Type } from './interfaces/type.interface';
 import { createApplication } from './application/nofault-application';
 import type { NofaultApplicationOptions } from './application/nofault-application';
+import type { NofaultApplication } from './application/nofault-application';
