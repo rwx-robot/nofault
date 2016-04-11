11c431480ee6b76fb461fad6ee1bc34b54bd5a09
Ny <johnnynode@gmail.com>
2016-04-11 14:39:24 +0000

    feat(core): add graceful shutdown signals

--- packages/core/src/nofault-factory.ts
+++ packages/core/src/nofault-factory.ts
@@ -1,3 +1,4 @@
 import type { DynamicModule } from './interfaces/module.interface';
 import type { Type } from './interfaces/type.interface';
 import { createApplication } from './application/nofault-application';
+import type { NofaultApplicationOptions } from './application/nofault-application';
