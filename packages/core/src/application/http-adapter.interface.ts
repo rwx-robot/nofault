af45581ab18c930cf4adbced45bac3d2a83c52d9
nofault maintainers <maintainers@nofault.io>
2016-04-11 11:56:19 +0000

    feat(core): add bootstrap hooks

--- packages/core/src/application/http-adapter.interface.ts
+++ packages/core/src/application/http-adapter.interface.ts
@@ -4,3 +4,5 @@ import type { IncomingMessage, ServerResponse } from 'node:http';
  * HTTP 请求处理函数（Node 原生签名）。
  *
  * 返回 `true` 表示"我已处理完这个请求"，处理链到此结束；
+ * 返回 `false`/`undefined` 表示"我不管"，继续交给下一个 handler。
+ */
