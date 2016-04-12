412981f37a966fa075d1e7af07fedd1b0801750b
nofault maintainers <maintainers@nofault.io>
2016-04-12 21:41:32 +0000

    feat(core): test http adapter abstraction

--- packages/core/src/application/nofault-application.ts
+++ packages/core/src/application/nofault-application.ts
@@ -46,3 +46,14 @@ export class NofaultApplication extends NofaultApplicationContext {
     }
     return this.adapter;
   }
+
+  /** 注册统一的请求处理入口（可多次调用，顺序组成链式兜底） */
+  use(handler: Parameters<HttpAdapter['useHandler']>[0]): this {
+    this.handlers.push(handler);
+    return this;
+  }
+
+  async listen(port: number, hostname = '0.0.0.0'): Promise<{ port: number; hostname: string }> {
+    const adapter = await this.getAdapter();
+    const handlers = [...this.handlers];
+    adapter.useHandler((req, res) => this.runHandlers(handlers, req, res));
