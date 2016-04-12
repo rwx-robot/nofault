7396f1e90083f912692d8cc0cb57902042924a55
nofault maintainers <maintainers@nofault.io>
2016-04-12 18:53:39 +0000

    feat(core): test bootstrap sequence

--- packages/core/src/application/nofault-application.ts
+++ packages/core/src/application/nofault-application.ts
@@ -8,3 +8,13 @@ export interface NofaultApplicationOptions extends ApplicationContextOptions {
   /** HTTP 适配器；未指定时尝试从 `@nofault/http` 自动加载 */
   httpAdapter?: HttpAdapter;
   /** 优雅退出超时（毫秒），超过后强制关闭 */
+  shutdownTimeout?: number;
+}
+
+/**
+ * nofault 应用：在上下文之上叠加 HTTP 监听与优雅退出能力。
+ *
+ * @example
+ * ```ts
+ * const app = await NofaultFactory.create(AppModule);
+ * await app.listen(3000);
