8be74d4063541c801b89b0139274b14cfd1d9b84
nofault maintainers <maintainers@nofault.io>
2016-04-13 16:51:58 +0000

    feat(core): polish init hooks

--- packages/core/src/nofault-factory.ts
+++ packages/core/src/nofault-factory.ts
@@ -2,3 +2,5 @@ import type { DynamicModule } from './interfaces/module.interface';
 import type { Type } from './interfaces/type.interface';
 import { createApplication } from './application/nofault-application';
 import type { NofaultApplicationOptions } from './application/nofault-application';
+import type { NofaultApplication } from './application/nofault-application';
+import { NofaultApplicationContext } from './application/nofault-application-context';
