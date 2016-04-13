dbde88f27798b6539e9e100903897cc0d104fa0b
nofault maintainers <maintainers@nofault.io>
2016-04-13 17:23:21 +0000

    feat(core): polish bootstrap hooks

--- packages/core/src/nofault-factory.ts
+++ packages/core/src/nofault-factory.ts
@@ -4,3 +4,4 @@ import { createApplication } from './application/nofault-application';
 import type { NofaultApplicationOptions } from './application/nofault-application';
 import type { NofaultApplication } from './application/nofault-application';
 import { NofaultApplicationContext } from './application/nofault-application-context';
+import type { ApplicationContextOptions } from './application/nofault-application-context';
