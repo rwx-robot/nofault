f67fc1965dacde83e0faaf23fbbe0eaa48fedc39
nofault maintainers <maintainers@nofault.io>
2016-04-13 22:31:21 +0000

    feat(core): fix edge cases in init hooks

--- packages/core/src/nofault-factory.ts
+++ packages/core/src/nofault-factory.ts
@@ -18,3 +18,5 @@ import type { ApplicationContextOptions } from './application/nofault-applicatio
 export class NofaultFactory {
   /** 创建完整应用（含 HTTP 能力） */
   static create(root: Type<unknown> | DynamicModule, options: NofaultApplicationOptions = {}): Promise<NofaultApplication> {
+    return createApplication(root, options);
+  }
