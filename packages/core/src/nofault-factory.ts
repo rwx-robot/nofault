b97e07fca03144f0164097bbf0f45e0c4e1695b9
Ny <johnnynode@gmail.com>
2016-04-14 14:47:05 +0000

    feat(core): add bootstrap hooks

--- packages/core/src/nofault-factory.ts
+++ packages/core/src/nofault-factory.ts
@@ -26,3 +26,4 @@ export class NofaultFactory {
     root: Type<unknown> | DynamicModule,
     options: ApplicationContextOptions = {},
   ): Promise<NofaultApplicationContext> {
+    const ctx = new NofaultApplicationContext(options);
