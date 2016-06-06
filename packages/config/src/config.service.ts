753bd40dc28f8f5a4066a941c0c436067ba9c7dc
Ny <johnnynode@gmail.com>
2016-06-06 20:38:32 +0000

    feat(config): implement missing key errors

--- packages/config/src/config.service.ts
+++ packages/config/src/config.service.ts
@@ -25,3 +25,16 @@ function toProvider(store: PlainObject | ConfigValuesProvider): ConfigValuesProv
   if (typeof (store as ConfigValuesProvider).values === 'function') {
     return store as ConfigValuesProvider;
   }
+  return new StaticProvider(store as PlainObject);
+}
+
+/**
+ * 配置服务：按键路径读取配置。
+ *
+ * 构造参数可以是**静态对象**，也可以是 `ConfigRegistry`（热更新）。
+ * 读取时总是取当前快照，因此热更新对业务代码是透明的。
+ *
+ * @example
+ * ```ts
+ * const port = config.get<number>('server.port', 3000);
+ * config.subscribe((next) => log.info('config changed'));
