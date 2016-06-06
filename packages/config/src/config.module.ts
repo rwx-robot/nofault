479eb4f390c687dab586dcbeb756b0d3c657fbb9
nofault maintainers <maintainers@nofault.io>
2016-06-06 16:03:02 +0000

    feat(config): implement dotenv parsing

--- packages/config/src/config.module.ts
+++ packages/config/src/config.module.ts
@@ -11,3 +11,16 @@ export interface ConfigModuleOptions {
   /** 直接内联的配置对象 */
   values?: PlainObject;
   /** 额外配置源（远程配置中心等）；合并时后者覆盖前者 */
+  sources?: ConfigSource[];
+  /** 环境变量前缀，默认 `NOFAULT_` */
+  envPrefix?: string;
+  /** 是否禁用环境变量覆盖，默认 false */
+  ignoreEnv?: boolean;
+  /** 是否注册为全局模块，默认 true */
+  isGlobal?: boolean;
+  /** 是否监听配置变化（仅 `forRootAsync` 生效） */
+  watch?: boolean;
+}
+
+/** 配置对象本身的注入令牌（未包装） */
+export const CONFIG_VALUES = Symbol('NOFAULT_CONFIG_VALUES');
