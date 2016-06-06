ab375af5958a97e52341d132b4e27ebdb11363ee
nofault maintainers <maintainers@nofault.io>
2016-06-06 21:49:04 +0000

    feat(config): refine dotenv parsing

--- packages/config/src/config.service.ts
+++ packages/config/src/config.service.ts
@@ -10,3 +10,15 @@ export class ConfigError extends Error {
 /** 取值来源：既可以是静态对象，也可以是会热更新的 registry */
 export interface ConfigValuesProvider {
   values(): PlainObject;
+  reload?(): Promise<PlainObject>;
+  subscribe?(listener: (values: PlainObject, previous: PlainObject) => void): () => void;
+}
+
+class StaticProvider implements ConfigValuesProvider {
+  constructor(private readonly store: PlainObject) {}
+  values(): PlainObject {
+    return this.store;
+  }
+}
+
+function toProvider(store: PlainObject | ConfigValuesProvider): ConfigValuesProvider {
