715dd522b88dc34493c4c6d3065a43daa893e1e9
Ny <johnnynode@gmail.com>
2016-06-06 23:08:36 +0000

    feat(config): refine config service accessors

--- packages/config/src/config.service.ts
+++ packages/config/src/config.service.ts
@@ -76,3 +76,17 @@ export class ConfigService {
   async reload(): Promise<void> {
     await this.provider.reload?.();
   }
+
+  /** 订阅配置变更（静态配置下永不触发） */
+  subscribe(listener: (values: PlainObject, previous: PlainObject) => void): () => void {
+    if (!this.provider.subscribe) return () => undefined;
+    return this.provider.subscribe(listener);
+  }
+
+  /** 是否支持热更新 */
+  get isReloadable(): boolean {
+    return typeof this.provider.reload === 'function';
+  }
+
+  private read(path: string): unknown {
+    let cursor: unknown = this.provider.values();
