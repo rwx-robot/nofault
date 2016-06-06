a524f31cb79f1a972328a802fc83198d38c9b033
nofault maintainers <maintainers@nofault.io>
2016-06-06 09:44:17 +0000

    feat(config): add config file loader

---
+++ packages/config/src/loader.ts
@@ -0,0 +1,89 @@
+import { existsSync, readFileSync } from 'node:fs';
+import { extname } from 'node:path';
+import { parse as parseYamlDocument } from 'yaml';
+
+export type PlainObject = Record<string, unknown>;
+
+/**
+ * 配置文件加载：JSON / YAML / .env。
+ *
+ * 遵循 12-factor：文件提供默认值，环境变量优先覆盖。
+ */
+
+export function loadConfigFile(filePath: string): PlainObject {
+  if (!existsSync(filePath)) {
+    throw new Error(`Config file not found: ${filePath}`);
+  }
+  const raw = readFileSync(filePath, 'utf8');
+  const ext = extname(filePath).toLowerCase();
+
+  switch (ext) {
+    case '.json':
+      return JSON.parse(raw) as PlainObject;
+    case '.yaml':
+    case '.yml':
+      return (parseYamlDocument(raw) ?? {}) as PlainObject;
+    case '.env':
+      return parseDotEnv(raw);
+    default:
+      // 无扩展名时按内容嗅探
+      return raw.trimStart().startsWith('{') ? (JSON.parse(raw) as PlainObject) : ((parseYamlDocument(raw) ?? {}) as PlainObject);
+  }
+}
+
+/** 解析 .env 文件 */
+export function parseDotEnv(raw: string): PlainObject {
+  const out: PlainObject = {};
+  for (const line of raw.split(/\r?\n/)) {
+    const t = line.trim();
+    if (t === '' || t.startsWith('#')) continue;
+    const idx = t.indexOf('=');
+    if (idx === -1) continue;
+    out[t.slice(0, idx).trim()] = t.slice(idx + 1).trim().replace(/^["']|["']$/g, '');
+  }
+  return out;
+}
+
+/**
+ * 用环境变量覆盖配置。
+ *
+ * 映射规则：`NOFAULT_SERVER__PORT=8080` → `server.port = 8080`
+ * （双下划线表示层级，单下划线表示键内下划线）
+ */
+export function applyEnvOverrides(config: PlainObject, prefix: string, source: NodeJS.ProcessEnv = process.env): PlainObject {
+  const normalizedPrefix = prefix.toUpperCase();
+  for (const [key, value] of Object.entries(source)) {
+    if (value === undefined) continue;
+    if (normalizedPrefix && !key.toUpperCase().startsWith(normalizedPrefix)) continue;
+    const pathKey = normalizedPrefix ? key.slice(normalizedPrefix.length) : key;
+    const segments = pathKey
+      .toLowerCase()
+      .replace(/__/g, '.')
+      .replace(/_/g, '.')
+      .split('.')
+      .filter(Boolean);
+    if (segments.length === 0) continue;
+    setDeep(config, segments, coerce(value));
+  }
+  return config;
+}
+
+function setDeep(target: PlainObject, segments: string[], value: unknown): void {
+  let cursor: PlainObject = target;
+  for (let i = 0; i < segments.length - 1; i++) {
+    const seg = segments[i]!;
+    const next = cursor[seg];
+    if (typeof next !== 'object' || next === null || Array.isArray(next)) {
+      cursor[seg] = {};
+    }
+    cursor = cursor[seg] as PlainObject;
+  }
+  cursor[segments[segments.length - 1]!] = value;
+}
+
+function coerce(v: string): unknown {
+  if (v === 'true') return true;
+  if (v === 'false') return false;
+  if (v !== '' && !Number.isNaN(Number(v))) return Number(v);
+  return v;
+}
---
+++ packages/config/src/registry.ts
@@ -0,0 +1,122 @@
+import type { PlainObject } from './loader';
+import { applyEnvOverrides } from './loader';
+import type { ConfigSource } from './provider';
+import { createFileSource, createInlineSource } from './provider';
+
+export type ConfigChangeListener = (values: PlainObject, previous: PlainObject) => void;
+
+export interface ConfigRegistryOptions {
+  sources?: ConfigSource[];
+  /** 环境变量前缀；传 '' 表示不启用 */
+  envPrefix?: string;
+}
+
+/**
+ * 配置注册表：聚合多个源、合并、应用环境变量覆盖、并在变更时通知订阅者。
+ *
+ * 合并顺序：**后面的源覆盖前面的**，环境变量永远最后且优先级最高（12-factor）。
+ */
+export class ConfigRegistry {
+  private readonly sources: ConfigSource[];
+  private readonly envPrefix: string;
+  private readonly listeners = new Set<ConfigChangeListener>();
+  private current: PlainObject = {};
+  private unwatchers: Array<() => void> = [];
+  private started = false;
+
+  constructor(options: ConfigRegistryOptions = {}) {
+    this.sources = options.sources ?? [];
+    this.envPrefix = options.envPrefix ?? 'NOFAULT_';
+  }
+
+  /** 首次加载 + 启动变更订阅 */
+  async start(): Promise<PlainObject> {
+    await this.reload();
+    if (this.started) return this.current;
+    this.started = true;
+    for (const source of this.sources) {
+      const unwatch = source.watch?.(() => {
+        void this.reload();
+      });
+      if (typeof unwatch === 'function') this.unwatchers.push(unwatch);
+    }
+    return this.current;
+  }
+
+  /** 重新聚合并（如有变化）通知订阅者 */
+  async reload(): Promise<PlainObject> {
+    let merged: PlainObject = {};
+    for (const source of this.sources) {
+      const part = await source.load();
+      merged = { ...merged, ...part };
+    }
+    if (this.envPrefix) {
+      merged = applyEnvOverrides(merged, this.envPrefix);
+    }
+    const previous = this.current;
+    const changed = JSON.stringify(previous) !== JSON.stringify(merged);
+    this.current = merged;
+    if (changed) {
+      for (const listener of this.listeners) {
+        try {
+          listener(merged, previous);
+        } catch (err) {
+          process.stderr.write(`[config] listener threw: ${String(err)}\n`);
+        }
+      }
+    }
+    return merged;
+  }
+
+  /**
+   * 当前配置。
+   *
+   * **不做克隆**——这个函数在 `ConfigService.get()` 里是**每次调用都走**的热路径，
+   * 每次 structuredClone 全量配置会把读配置变成 O(配置大小) 的操作（实测是 v0.3.0 最大的一处开销）。
+   *
+   * 安全性由"整体替换"保证：`reload()` 每次生成全新对象，从不在原地修改，
+   * 所以持有旧引用不会看到撕裂状态。需要副本时请用 `ConfigService.snapshot()`。
+   */
+  values(): PlainObject {
+    return this.current;
+  }
+
+  /** 订阅变更（`@OnConfigUpdate()` 与日志/连接池等需要感知变化的组件用） */
+  subscribe(listener: ConfigChangeListener): () => void {
+    this.listeners.add(listener);
+    return () => this.listeners.delete(listener);
+  }
+
+  get listenerCount(): number {
+    return this.listeners.size;
+  }
+
+  /** 关闭：停止所有 watcher 与定时器 */
+  async close(): Promise<void> {
+    for (const off of this.unwatchers) off();
+    this.unwatchers = [];
+    for (const source of this.sources) source.dispose?.();
+    this.started = false;
+  }
+}
+
+export interface BuildRegistryOptions {
+  path?: string;
+  values?: PlainObject;
+  sources?: ConfigSource[];
+  envPrefix?: string;
+  ignoreEnv?: boolean;
+  watch?: boolean;
+}
+
+/** 按传统 options 组装 registry（保持 `forRoot` 的向后兼容） */
+export function buildRegistry(options: BuildRegistryOptions): ConfigRegistry {
+  const sources: ConfigSource[] = [...(options.sources ?? [])];
+  if (options.values) sources.push(createInlineSource(options.values));
+  if (options.path) sources.push(createFileSource({ path: options.path, watch: options.watch ?? false }));
+
+  return new ConfigRegistry({
+    sources,
+    envPrefix: options.ignoreEnv ? '' : (options.envPrefix ?? 'NOFAULT_'),
+  });
+}
