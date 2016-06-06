de7ca7c4c11d20f49ba67467d196843861c72eb5
nofault maintainers <maintainers@nofault.io>
2016-06-06 18:07:07 +0000

    feat(config): implement dynamic config module

--- packages/config/src/config.module.ts
+++ packages/config/src/config.module.ts
@@ -50,3 +50,15 @@ export class ConfigModule {
 
     return {
       module: ConfigModule,
+      global: options.isGlobal ?? true,
+      providers,
+      exports: [ConfigService, CONFIG_VALUES],
+    };
+  }
+
+  /**
+   * 异步注册：完成首次加载后再返回模块定义，**支持热更新**。
+   *
+   * 内核的模块扫描器会 `await` 动态模块，所以直接放进 `imports` 即可。
+   *
+   * @example
