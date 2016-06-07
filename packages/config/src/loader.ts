4e537ef58a9fed212513b0f71eefabceed9c5b74
Ny <johnnynode@gmail.com>
2016-06-07 15:29:24 +0000

    feat(config): harden dynamic config module

--- packages/config/src/loader.ts
+++ packages/config/src/loader.ts
@@ -21,3 +21,14 @@ export function loadConfigFile(filePath: string): PlainObject {
     case '.json':
       return JSON.parse(raw) as PlainObject;
     case '.yaml':
+    case '.yml':
+      return (parseYamlDocument(raw) ?? {}) as PlainObject;
+    case '.env':
+      return parseDotEnv(raw);
+    default:
+      // 无扩展名时按内容嗅探
+      return raw.trimStart().startsWith('{') ? (JSON.parse(raw) as PlainObject) : ((parseYamlDocument(raw) ?? {}) as PlainObject);
+  }
+}
+
+/** 解析 .env 文件 */
