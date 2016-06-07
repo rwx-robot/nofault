05dc6789f29bceafb37dd41582bbfdcc82336da5
Ny <johnnynode@gmail.com>
2016-06-07 09:29:55 +0000

    feat(config): refine dynamic config module

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -2,21 +2,3 @@
  * @nofault/config —— 配置：文件加载、环境变量覆盖、命名空间、热更新。
  */
 export { ConfigModule, registerAs, CONFIG_VALUES, CONFIG_REGISTRY } from './config.module';
-export type { ConfigModuleOptions } from './config.module';
-
-export { ConfigService, createConfigService, ConfigError } from './config.service';
-export type { ConfigValuesProvider } from './config.service';
-
-export { ConfigRegistry, buildRegistry } from './registry';
-export type { ConfigChangeListener, ConfigRegistryOptions } from './registry';
-
-export {
-  createInlineSource,
-  createFileSource,
-  createPollingSource,
-  createEnvSource,
-} from './provider';
-export type { ConfigSource, FileSourceOptions, PollingSourceOptions } from './provider';
-
-export { loadConfigFile, applyEnvOverrides, parseDotEnv } from './loader';
-export type { PlainObject } from './loader';
