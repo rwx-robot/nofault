9a3e3c2e39b9385740c24b262c8cb999b479c83a
nofault maintainers <maintainers@nofault.io>
2016-06-07 19:10:40 +0000

    feat(config): test dotenv parsing

--- packages/config/src/loader.ts
+++ packages/config/src/loader.ts
@@ -8,3 +8,13 @@ export type PlainObject = Record<string, unknown>;
  * 配置文件加载：JSON / YAML / .env。
  *
  * 遵循 12-factor：文件提供默认值，环境变量优先覆盖。
+ */
+
+export function loadConfigFile(filePath: string): PlainObject {
+  if (!existsSync(filePath)) {
+    throw new Error(`Config file not found: ${filePath}`);
+  }
+  const raw = readFileSync(filePath, 'utf8');
+  const ext = extname(filePath).toLowerCase();
+
+  switch (ext) {
