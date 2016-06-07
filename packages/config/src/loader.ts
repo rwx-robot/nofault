983eb6c4057fd2bae8462cec2d24608f329142a1
nofault maintainers <maintainers@nofault.io>
2016-06-07 20:17:54 +0000

    feat(config): test config service accessors

--- packages/config/src/loader.ts
+++ packages/config/src/loader.ts
@@ -28,3 +28,13 @@ export function loadConfigFile(filePath: string): PlainObject {
     default:
       // 无扩展名时按内容嗅探
       return raw.trimStart().startsWith('{') ? (JSON.parse(raw) as PlainObject) : ((parseYamlDocument(raw) ?? {}) as PlainObject);
+  }
+}
+
+/** 解析 .env 文件 */
+export function parseDotEnv(raw: string): PlainObject {
+  const out: PlainObject = {};
+  for (const line of raw.split(/\r?\n/)) {
+    const t = line.trim();
+    if (t === '' || t.startsWith('#')) continue;
+    const idx = t.indexOf('=');
