e665343ccb21428780689935035b637b4fc57d92
Ny <johnnynode@gmail.com>
2016-06-07 10:20:48 +0000

    feat(config): refine namespaced registerAs helper

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -2,3 +2,6 @@
  * @nofault/config —— 配置：文件加载、环境变量覆盖、命名空间、热更新。
  */
 export { ConfigModule, registerAs, CONFIG_VALUES, CONFIG_REGISTRY } from './config.module';
+export type { ConfigModuleOptions } from './config.module';
+
+export { ConfigService, createConfigService, ConfigError } from './config.service';
