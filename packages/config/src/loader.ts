a5f92ceada7d926092208298050df8e5da627c10
Ny <johnnynode@gmail.com>
2016-06-07 14:34:08 +0000

    feat(config): harden config service accessors

--- packages/config/src/loader.ts
+++ packages/config/src/loader.ts
@@ -9,3 +9,15 @@ export type PlainObject = Record<string, unknown>;
  *
  * 遵循 12-factor：文件提供默认值，环境变量优先覆盖。
  */
+
+export function loadConfigFile(filePath: string): PlainObject {
+  if (!existsSync(filePath)) {
+    throw new Error(`Config file not found: ${filePath}`);
+  }
+  const raw = readFileSync(filePath, 'utf8');
+  const ext = extname(filePath).toLowerCase();
+
+  switch (ext) {
+    case '.json':
+      return JSON.parse(raw) as PlainObject;
+    case '.yaml':
