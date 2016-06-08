4a98693943c77ec26b68b0a4305e4e32214c64f6
nofault maintainers <maintainers@nofault.io>
2016-06-08 18:11:05 +0000

    feat(config): polish dynamic config module

--- packages/config/src/registry.ts
+++ packages/config/src/registry.ts
@@ -53,3 +53,17 @@ export class ConfigRegistry {
     if (this.envPrefix) {
       merged = applyEnvOverrides(merged, this.envPrefix);
     }
+    const previous = this.current;
+    const changed = JSON.stringify(previous) !== JSON.stringify(merged);
+    this.current = merged;
+    if (changed) {
+      for (const listener of this.listeners) {
+        try {
+          listener(merged, previous);
+        } catch (err) {
+          process.stderr.write(`[config] listener threw: ${String(err)}\n`);
+        }
+      }
+    }
+    return merged;
+  }
