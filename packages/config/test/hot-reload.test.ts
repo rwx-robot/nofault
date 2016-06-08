d0bc364c4e01654053a190a36163f3c840d9dfa3
Ny <johnnynode@gmail.com>
2016-06-08 20:28:52 +0000

    feat(config): polish missing key errors

--- packages/config/test/hot-reload.test.ts
+++ packages/config/test/hot-reload.test.ts
@@ -44,3 +44,26 @@ describe('ConfigRegistry', () => {
   it('values() is the live object; ConfigService.snapshot() is the defensive copy', async () => {
     const registry = new ConfigRegistry({ sources: [createInlineSource({ a: { b: 1 } })], envPrefix: '' });
     await registry.start();
+
+    // values() 是热路径，故意不克隆（见 registry.ts 注释）
+    const live = registry.values();
+    (live.a as { b: number }).b = 99;
+    expect(registry.values().a).toEqual({ b: 99 });
+
+    // 需要"不可变快照"时用 ConfigService.snapshot()
+    const config = createConfigService(registry);
+    const copy = config.snapshot();
+    (copy.a as { b: number }).b = 7;
+    expect(registry.values().a).toEqual({ b: 99 });
+  });
+
+  it('reload replaces the whole object so stale references never see torn state', async () => {
+    let v = 1;
+    const registry = new ConfigRegistry({ sources: [{ name: 'live', load: () => ({ v }) }], envPrefix: '' });
+    await registry.start();
+    const before = registry.values();
+    v = 2;
+    await registry.reload();
+    expect(before.v).toBe(1);
+    expect(registry.values().v).toBe(2);
+  });
