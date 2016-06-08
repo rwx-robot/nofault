092d7e03ab988adfe696c0877bfc4938825e27ee
Ny <johnnynode@gmail.com>
2016-06-08 14:50:08 +0000

    feat(config): document missing key errors

--- packages/config/test/config.test.ts
+++ packages/config/test/config.test.ts
@@ -12,3 +12,18 @@ import {
 } from '../src/index';
 
 import { Injectable, Module, NofaultFactory, Inject } from '@nofault/core';
+
+function writeTemp(name: string, content: string): string {
+  const dir = mkdtempSync(join(tmpdir(), 'nofault-cfg-'));
+  const file = join(dir, name);
+  writeFileSync(file, content, 'utf8');
+  return file;
+}
+
+describe('ConfigService', () => {
+  it('reads nested values by dot path', () => {
+    const cfg = createConfigService({ server: { port: 3000, host: '0.0.0.0' } });
+    expect(cfg.get<number>('server.port')).toBe(3000);
+    expect(cfg.get('server.missing', 'fallback')).toBe('fallback');
+    expect(cfg.has('server.host')).toBe(true);
+    expect(() => cfg.getOrThrow('nope')).toThrow();
