12b7052b831b0aca79de27bd9c913a0a26766f21
nofault maintainers <maintainers@nofault.io>
2016-06-08 16:13:28 +0000

    feat(config): polish dotenv parsing

--- packages/config/src/registry.ts
+++ packages/config/src/registry.ts
@@ -12,3 +12,17 @@ export interface ConfigRegistryOptions {
 }
 
 /**
+ * 配置注册表：聚合多个源、合并、应用环境变量覆盖、并在变更时通知订阅者。
+ *
+ * 合并顺序：**后面的源覆盖前面的**，环境变量永远最后且优先级最高（12-factor）。
+ */
+export class ConfigRegistry {
+  private readonly sources: ConfigSource[];
+  private readonly envPrefix: string;
+  private readonly listeners = new Set<ConfigChangeListener>();
+  private current: PlainObject = {};
+  private unwatchers: Array<() => void> = [];
+  private started = false;
+
+  constructor(options: ConfigRegistryOptions = {}) {
+    this.sources = options.sources ?? [];
