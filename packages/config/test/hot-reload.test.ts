19121e5b5bdc33ef8a6a971e0d6c0e6c770c1ab8
Ny <johnnynode@gmail.com>
2016-06-08 19:31:44 +0000

    feat(config): polish deep snapshot isolation

--- packages/config/test/hot-reload.test.ts
+++ packages/config/test/hot-reload.test.ts
@@ -22,3 +22,25 @@ const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 describe('ConfigRegistry', () => {
   it('merges sources in order, later wins', async () => {
+    const registry = new ConfigRegistry({
+      sources: [createInlineSource({ a: 1, b: 1 }), createInlineSource({ b: 2 })],
+      envPrefix: '',
+    });
+    const values = await registry.start();
+    expect(values).toEqual({ a: 1, b: 2 });
+  });
+
+  it('applies env overrides last', async () => {
+    const registry = new ConfigRegistry({
+      sources: [createInlineSource({ server: { port: 3000 } })],
+      envPrefix: 'TEST_',
+    });
+    process.env.TEST_SERVER__PORT = '8080';
+    const values = await registry.start();
+    expect(values.server).toEqual({ port: 8080 });
+    delete process.env.TEST_SERVER__PORT;
+  });
+
+  it('values() is the live object; ConfigService.snapshot() is the defensive copy', async () => {
+    const registry = new ConfigRegistry({ sources: [createInlineSource({ a: { b: 1 } })], envPrefix: '' });
+    await registry.start();
