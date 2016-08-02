be69064c2b9aef4027ac34a53db36a849890fc4b
nofault maintainers <maintainers@nofault.io>
2016-08-02 21:00:21 +0000

    feat(logger): test log levels

--- packages/logger/src/log-record.ts
+++ packages/logger/src/log-record.ts
@@ -6,3 +6,11 @@ export interface LogRecord {
   level: LogLevel;
   levelName: string;
   message: string;
+  context?: string;
+  /** 结构化字段 */
+  fields?: Record<string, unknown>;
+  /** 错误堆栈（error/fatal 时填充） */
+  error?: { name: string; message: string; stack?: string };
+}
+
+const COLORS: Record<string, string> = {
