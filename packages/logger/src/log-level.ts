3acc593de498343656a38c13f93f26a34914d34d
Ny <johnnynode@gmail.com>
2016-08-02 15:21:42 +0000

    feat(logger): harden log record shape

--- packages/logger/src/log-level.ts
+++ packages/logger/src/log-level.ts
@@ -29,3 +29,9 @@ const NAME_TO_LEVEL: Record<string, LogLevel> = {
 export function levelName(level: LogLevel): string {
   return LEVEL_NAMES[level] ?? 'info';
 }
+
+export function parseLevel(value: string | number | undefined, fallback = LogLevel.INFO): LogLevel {
+  if (typeof value === 'number') return value as LogLevel;
+  if (typeof value === 'string') {
+    const key = value.toLowerCase();
+    if (key in NAME_TO_LEVEL) return NAME_TO_LEVEL[key]!;
