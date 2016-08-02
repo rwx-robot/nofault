2fe909e12d4b3047093e160f7085f30626d6981f
Ny <johnnynode@gmail.com>
2016-08-02 16:55:07 +0000

    feat(logger): harden console transport

--- packages/logger/src/log-record.ts
+++ packages/logger/src/log-record.ts
@@ -8,3 +8,13 @@ export interface LogRecord {
   message: string;
   context?: string;
   /** 结构化字段 */
+  fields?: Record<string, unknown>;
+  /** 错误堆栈（error/fatal 时填充） */
+  error?: { name: string; message: string; stack?: string };
+}
+
+const COLORS: Record<string, string> = {
+  trace: '\x1b[90m',
+  debug: '\x1b[36m',
+  info: '\x1b[32m',
+  warn: '\x1b[33m',
