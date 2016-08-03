39aafe168cfda774b68663925cb83685b25587ff
Ny <johnnynode@gmail.com>
2016-08-03 17:37:05 +0000

    feat(logger): document pretty formatter

--- packages/logger/test/logger.test.ts
+++ packages/logger/test/logger.test.ts
@@ -11,3 +11,15 @@ import {
 import type { LogRecord } from '../src/log-record';
 
 const fakeStream = (): { stream: NodeJS.WriteStream; write: ReturnType<typeof vi.fn> } => {
+  const write = vi.fn();
+  return { stream: { write } as unknown as NodeJS.WriteStream, write };
+};
+
+const recordAt = (level: LogLevel): LogRecord =>
+  ({ timestamp: '', level, levelName: LogLevel[level].toLowerCase(), message: 'x' }) as LogRecord;
+
+describe('ConsoleTransport', () => {
+  it('routes error and above to the error stream, the rest to the normal stream', () => {
+    const out = fakeStream();
+    const err = fakeStream();
+    const t = new ConsoleTransport(out.stream, err.stream);
