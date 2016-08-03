d71423c41f7516e178897397120eccdc968a41ef
nofault maintainers <maintainers@nofault.io>
2016-08-03 13:00:22 +0000

    feat(logger): document log levels

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -19,3 +19,23 @@ export interface LoggerOptions {
    * 上下文提供者：每条日志调用一次，把返回值合并进 fields。
    *
    * 典型用法是注入请求上下文，让**每一行日志自动带上 traceId**：
+   * ```ts
+   * createLogger({ contextProvider: () => currentContext()?.toJSON() })
+   * ```
+   * 放在这里而不是让 logger 依赖 `@nofault/context`，是为了保持 logger 零依赖。
+   */
+  contextProvider?: () => Record<string, unknown> | undefined | null;
+  /**
+   * 采样：只对**低于** `sampleBelow` 的级别生效（默认 INFO，即只采样 debug/trace）。
+   *
+   * 高频调试日志用它降本；`sampleBelow` 及以上永远不打折扣——
+   * 问题排查最需要的就是 warn/error，那些不能丢。
+   */
+  sampling?: { rate: number; sampleBelow?: LogLevel };
+}
+
+/** 标准输出传输 */
+export class ConsoleTransport implements LogTransport {
+  constructor(
+    private readonly stream: NodeJS.WriteStream = process.stdout,
+  ) {}
