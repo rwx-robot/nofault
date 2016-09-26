2fec694cbed6f8a3b39aeb605fac1a8c54f68c95
Ny <johnnynode@gmail.com>
2016-09-26 15:22:11 +0000

    feat(http): implement http adapter contract

--- packages/http/src/create-http-application.ts
+++ packages/http/src/create-http-application.ts
@@ -1,32 +1,3 @@
 import { NofaultApplication, createApplication } from '@nofault/core';
 import type { DynamicModule, Type } from '@nofault/core';
 import { NodeHttpAdapter } from './node-http-adapter';
-
-export interface HttpApplicationOptions {
-  name?: string;
-  quiet?: boolean;
-  shutdownTimeout?: number;
-  /** 复用外部传入的适配器（测试常用） */
-  httpAdapter?: import('@nofault/core').HttpAdapter;
-}
-
-/**
- * 创建一个带 `node:http` 适配器的 nofault 应用。
- *
- * 这是 v0.1.0 的推荐入口，对应 NestJS 里 `NestFactory.create(AppModule)` 的角色。
- *
- * @example
- * ```ts
- * const app = await createHttpApplication(AppModule);
- * await app.listen(3000);
- * ```
- */
-export function createHttpApplication(
-  root: Type<unknown> | DynamicModule,
-  options: HttpApplicationOptions = {},
-): Promise<NofaultApplication> {
-  return createApplication(root, {
-    ...options,
-    httpAdapter: options.httpAdapter ?? new NodeHttpAdapter(),
-  });
-}
