532fbefd4a4ec12099961f07ae38706e8fa8b375
Ny <johnnynode@gmail.com>
2016-09-26 17:21:42 +0000

    feat(http): implement json response helper

--- packages/http/src/create-http-application.ts
+++ packages/http/src/create-http-application.ts
@@ -8,3 +8,7 @@ export interface HttpApplicationOptions {
   shutdownTimeout?: number;
   /** 复用外部传入的适配器（测试常用） */
   httpAdapter?: import('@nofault/core').HttpAdapter;
+}
+
+/**
+ * 创建一个带 `node:http` 适配器的 nofault 应用。
