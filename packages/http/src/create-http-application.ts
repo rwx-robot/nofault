27446a79a8a4bd390bb5c891b93569f19929f576
nofault maintainers <maintainers@nofault.io>
2016-09-26 17:21:42 +0000

    feat(http): implement json response helper

--- packages/http/src/create-http-application.ts
+++ packages/http/src/create-http-application.ts
@@ -1,3 +1,6 @@
 import { NofaultApplication, createApplication } from '@nofault/core';
 import type { DynamicModule, Type } from '@nofault/core';
 import { NodeHttpAdapter } from './node-http-adapter';
+
+export interface HttpApplicationOptions {
+  name?: string;
