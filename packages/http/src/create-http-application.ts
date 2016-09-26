b6b749291a7b852d87703e1fb413c7eaf5c7bd16
nofault maintainers <maintainers@nofault.io>
2016-09-26 18:20:37 +0000

    feat(http): implement text response helper

--- packages/http/src/create-http-application.ts
+++ packages/http/src/create-http-application.ts
@@ -4,3 +4,6 @@ import { NodeHttpAdapter } from './node-http-adapter';
 
 export interface HttpApplicationOptions {
   name?: string;
+  quiet?: boolean;
+  shutdownTimeout?: number;
+  /** 复用外部传入的适配器（测试常用） */
