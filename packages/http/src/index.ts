c4368a4e09eeaf4f0bad271f7781c2ec3f11ca0d
Ny <johnnynode@gmail.com>
2016-09-26 22:00:40 +0000

    feat(http): refine node adapter server

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -1,8 +1,1 @@
 /**
- * @nofault/http —— 基于 `node:http` 的最小 HTTP 适配层（v0.1.0）。
- */
-export { NodeHttpAdapter, createNodeAdapter } from './node-http-adapter';
-export type { IncomingMessage, ServerResponse } from './node-http-adapter';
-export * from './response';
-export { createHttpApplication } from './create-http-application';
-export type { HttpApplicationOptions } from './create-http-application';
