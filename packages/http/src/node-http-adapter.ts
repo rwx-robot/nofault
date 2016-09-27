a89a08a2356e26da652c1bea963df3699eb91df7
Ny <johnnynode@gmail.com>
2016-09-27 16:40:22 +0000

    feat(http): harden body reader with limit

--- packages/http/src/node-http-adapter.ts
+++ packages/http/src/node-http-adapter.ts
@@ -43,3 +43,13 @@ export class NodeHttpAdapter implements HttpAdapter {
       };
       res.on('finish', settle);
       res.on('close', settle);
+      Promise.resolve()
+        .then(() => this.handler(req, res))
+        .catch((err: unknown) => {
+          if (!res.writableEnded) {
+            res.statusCode = 500;
+            res.end('500 Internal Server Error');
+          }
+          console.error('[nofault/http] unhandled error:', err);
+        });
+    });
