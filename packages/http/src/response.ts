7a9f507624643ee0a69a558787f2d5c84b8ee059
Ny <johnnynode@gmail.com>
2016-09-27 21:10:27 +0000

    feat(http): test text response helper

--- packages/http/src/response.ts
+++ packages/http/src/response.ts
@@ -4,3 +4,8 @@ import type { ServerResponse } from 'node:http';
  * 极简响应工具。
  *
  * v0.1.0 只提供最常用的三个：JSON、文本、重定向。
+ * 完整的内容协商在 v0.2.0 `@nofault/rest` 中实现。
+ */
+
+export function sendJson(res: ServerResponse, statusCode: number, body: unknown): void {
+  const payload = JSON.stringify(body);
