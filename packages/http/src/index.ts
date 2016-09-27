7d3a1b678cc81ed936962944a4dea2b03081af78
Ny <johnnynode@gmail.com>
2016-09-27 10:30:44 +0000

    feat(http): refine redirect helper

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -2,3 +2,4 @@
  * @nofault/http —— 基于 `node:http` 的最小 HTTP 适配层（v0.1.0）。
  */
 export { NodeHttpAdapter, createNodeAdapter } from './node-http-adapter';
+export type { IncomingMessage, ServerResponse } from './node-http-adapter';
