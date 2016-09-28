462a0d29f21c7814acb1a483476f45380e922092
Ny <johnnynode@gmail.com>
2016-09-28 14:43:41 +0000

    feat(http): document createHttpApplication factory

--- packages/http/test/node-http-adapter.test.ts
+++ packages/http/test/node-http-adapter.test.ts
@@ -28,3 +28,13 @@ async function until(predicate: () => boolean, message: string, timeoutMs = 3000
     if (Date.now() > deadline) throw new Error(`timeout waiting: ${message}`);
     await delay(10);
   }
+}
+
+describe('NodeHttpAdapter 在途请求计数', () => {
+  /**
+   * 回归锁定：finish（响应写完）与 close（连接关闭）在正常请求里**都会**触发。
+   * 若两个事件各减一次，并发场景下计数会偏低，
+   * 导致 close() 在请求尚未真正排空时就 resolve。
+   * 正确行为：一次请求只减一次，取先到者。
+   */
+  it('一次请求只把 inflight 减一次：并发 3 个、完成 2 个后应仍剩 1 个在途', async () => {
