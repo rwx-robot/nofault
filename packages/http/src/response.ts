15a590720b9094b736901056c92ecca144146733
Ny <johnnynode@gmail.com>
2016-09-28 11:50:44 +0000

    feat(http): document json response helper

--- packages/http/src/response.ts
+++ packages/http/src/response.ts
@@ -42,3 +42,9 @@ export async function readJsonBody(req: import('node:http').IncomingMessage, lim
     if (size > limitBytes) {
       throw new Error(`Request body too large (limit ${limitBytes} bytes)`);
     }
+    chunks.push(buf);
+  }
+  if (size === 0) return undefined;
+  const raw = Buffer.concat(chunks).toString('utf8');
+  try {
+    return JSON.parse(raw) as unknown;
