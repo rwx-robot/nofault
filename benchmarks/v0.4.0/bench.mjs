83d837e30292cd01dc1798e2d250c7e7dfd080b1
nofault maintainers <maintainers@nofault.io>
2016-11-14 20:56:16 +0000

    docs(v0.1.0): implement greeter service

--- benchmarks/v0.4.0/bench.mjs
+++ benchmarks/v0.4.0/bench.mjs
@@ -120,3 +120,63 @@ function measure(name, fn, iterations) {
 
 function round(n) {
   return Math.round(n * 100) / 100;
+}
+
+// ---------------------------------------------------------------- 主流程
+
+const { parseContract } = await import(join(repoRoot, 'packages/parser/dist/index.js'));
+const { generate, writeFiles } = await import(join(repoRoot, 'packages/codegen/dist/index.js'));
+
+console.log(`\n[v0.4.0] codegen benchmark — ${opts.iterations} iterations\n`);
+
+const parseApi = measure('parse .api', () => parseContract(API_CONTRACT, 'bench.api', 'api'), opts.iterations);
+const parseTs = measure('parse .api.ts', () => parseContract(TS_CONTRACT, 'bench.api.ts', 'ts'), opts.iterations);
+
+const specFromApi = parseContract(API_CONTRACT, 'bench.api', 'api');
+const specFromTs = parseContract(TS_CONTRACT, 'bench.api.ts', 'ts');
+
+const genApi = measure('generate (from .api)', () => generate(specFromApi, { rootModule: true }), opts.iterations);
+const genTs = measure('generate (from .api.ts)', () => generate(specFromTs, { rootModule: true }), opts.iterations);
+
+// 端到端：解析 + 生成 + 写盘
+const files = generate(specFromApi, { rootModule: true }).files;
+const e2e = measure(
+  'end-to-end (parse+generate+write)',
+  () => {
+    const dir = mkdtempSync(join(tmpdir(), 'nf-bench-'));
+    const spec = parseContract(API_CONTRACT, 'bench.api', 'api');
+    writeFiles(generate(spec, { rootModule: true }).files, { outDir: join(dir, 'src') });
+    rmSync(dir, { recursive: true, force: true });
+  },
+  Math.max(10, Math.round(opts.iterations / 10)),
+);
+
+// 杠杆率：契约行数 vs 生成行数
+const contractLines = API_CONTRACT.split('\n').length;
+const generatedLines = files.reduce((n, f) => n + f.content.split('\n').length, 0);
+
+const micro = { parseApi, parseTs, genApi, genTs, e2e };
+const leverage = {
+  contractLines,
+  generatedFiles: files.length,
+  generatedLines,
+  ratio: round(generatedLines / contractLines),
+};
+
+console.log('A. 解析');
+console.log(`   .api        ${parseApi.meanMs} ms  (${parseApi.opsPerSec} ops/sec, p95 ${parseApi.p95Ms} ms)`);
+console.log(`   .api.ts     ${parseTs.meanMs} ms  (${parseTs.opsPerSec} ops/sec, p95 ${parseTs.p95Ms} ms)`);
+console.log('B. 生成');
+console.log(`   from .api   ${genApi.meanMs} ms  (${genApi.opsPerSec} ops/sec)`);
+console.log(`   from .api.ts${genTs.meanMs} ms  (${genTs.opsPerSec} ops/sec)`);
+console.log('C. 端到端（含写盘）');
+console.log(`   ${e2e.meanMs} ms  (p95 ${e2e.p95Ms} ms)`);
+console.log(`\n杠杆率：${contractLines} 行契约 -> ${generatedLines} 行代码 / ${files.length} 个文件（${leverage.ratio}x）\n`);
+
+if (opts.report) {
+  writeFileSync(
+    join(here, 'results.json'),
+    JSON.stringify({ generatedAt: new Date().toISOString(), micro, leverage }, null, 2),
+  );
+  console.log('结果已写入 benchmarks/v0.4.0/results.json');
+}
