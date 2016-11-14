4c6939cb0b1737944c00bb35596fb1c4654c76d4
Ny <johnnynode@gmail.com>
2016-11-14 20:56:16 +0000

    docs(v0.1.0): implement greeter service

--- benchmarks/v0.5.0/bench.mjs
+++ benchmarks/v0.5.0/bench.mjs
@@ -57,3 +57,62 @@ async function bench(name, fn, iterations = opts.iterations) {
     samples.push(Number(process.hrtime.bigint() - t) / 1e6);
   }
   samples.sort((a, b) => a - b);
+  const mean = samples.reduce((a, b) => a + b, 0) / samples.length;
+  const p95 = samples[Math.floor(samples.length * 0.95)];
+  return { name, meanMs: round(mean), p95Ms: round(p95), opsPerSec: round(1000 / mean) };
+}
+
+function round(n) {
+  return Math.round(n * 100) / 100;
+}
+
+const insert = await bench('insert', async (i) => {
+  const row = new Row();
+  row.name = `user-${i}`;
+  row.score = i % 100;
+  await repo.save(row);
+});
+
+const findById = await bench('findById', async (i) => {
+  await repo.findById((i % 500) + 1);
+});
+
+const findWithQuery = await bench('query (where + order + limit)', async () => {
+  await repo.createQueryBuilder().andWhere('score', '>=', 50).orderBy('score', 'DESC').limit(10).getMany();
+});
+
+const paginate = await bench('paginate', async (i) => {
+  await repo.paginate((i % 5) + 1, 20);
+});
+
+const transaction = await bench('transaction (commit)', async () => {
+  await source.transaction(async () => {
+    const row = new Row();
+    row.name = 'tx';
+    row.score = 1;
+    await repo.save(row);
+  });
+});
+
+// ------------------------------------------------------------------ 缓存
+const cache = new MemoryCache({ ttl: 60_000, max: 10_000 });
+const nullCache = new NullCache();
+
+// 回源统一带 1ms 模拟延迟。
+// 不加延迟的话两边都是 0ms，"加速比"算出来是 0x —— 测了个寂寞。
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+const load = async (i) => {
+  await sleep(1);
+  return { v: i };
+};
+
+// 先预热：不预热的话每次都是 miss，量到的其实是"回源延迟"
+for (let i = 0; i < 100; i++) await cache.getOrSet(`k:${i}`, () => load(i));
+
+const cacheHit = await bench('cache get (hit)', async (i) => {
+  await cache.getOrSet(`k:${i % 100}`, () => load(i));
+});
+
+const cacheMiss = await bench('loader only (no cache)', async (i) => {
+  await nullCache.getOrSet(`k:${i % 100}`, () => load(i));
+});
