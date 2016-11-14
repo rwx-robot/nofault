42dc611f0d67417c1efd5f0e0a805882a31ad2ff
Ny <johnnynode@gmail.com>
2016-11-14 15:57:59 +0000

    docs(v0.1.0): add architecture diagram

--- benchmarks/v0.2.0/bench.mjs
+++ benchmarks/v0.2.0/bench.mjs
@@ -111,3 +111,114 @@ function once(port, path, agents, method = 'GET', body) {
         path,
         method,
         agent,
+        headers: body ? { 'content-type': 'application/json', 'content-length': Buffer.byteLength(body) } : undefined,
+      },
+      (response) => {
+        response.on('data', () => {});
+        response.on('end', () => res(Number(process.hrtime.bigint() - start) / 1e6));
+      },
+    );
+    req.on('error', () => res(-1));
+    if (body) req.write(body);
+    req.end();
+  });
+}
+
+async function run(port, scenario, { duration, connections, warmup }) {
+  const agents = Array.from(
+    { length: Math.max(1, Math.ceil(connections / 16)) },
+    () => new Agent({ keepAlive: true, maxSockets: 16, maxFreeSockets: 16 }),
+  );
+  const warmEnd = Date.now() + warmup * 1000;
+  while (Date.now() < warmEnd) await once(port, scenario.path, agents, scenario.method, scenario.body);
+
+  const latencies = [];
+  const end = Date.now() + duration * 1000;
+  const started = Date.now();
+  await Promise.all(
+    Array.from({ length: connections }, () => (async () => {
+      while (Date.now() < end) {
+        const l = await once(port, scenario.path, agents, scenario.method, scenario.body);
+        if (l >= 0) latencies.push(l);
+      }
+    })()),
+  );
+  const elapsed = (Date.now() - started) / 1000;
+  for (const a of agents) a.destroy();
+  return summarize(latencies, elapsed);
+}
+
+function summarize(latencies, elapsedSec) {
+  const sorted = [...latencies].sort((a, b) => a - b);
+  const q = (p) => (sorted.length ? sorted[Math.min(sorted.length - 1, Math.floor(sorted.length * p))] : 0);
+  const total = sorted.reduce((s, v) => s + v, 0);
+  return {
+    requests: sorted.length,
+    rps: Number((sorted.length / elapsedSec).toFixed(1)),
+    meanMs: Number((total / Math.max(1, sorted.length)).toFixed(3)),
+    p50Ms: Number(q(0.5).toFixed(3)),
+    p95Ms: Number(q(0.95).toFixed(3)),
+    p99Ms: Number(q(0.99).toFixed(3)),
+    maxMs: Number((sorted[sorted.length - 1] ?? 0).toFixed(3)),
+  };
+}
+
+function pickMedianRound(rounds) {
+  return [...rounds].sort((a, b) => a.rps - b.rps)[Math.floor(rounds.length / 2)];
+}
+
+const SCENARIOS = [
+  { name: 'GET /api/users/1 (routing + param)', path: '/api/users/1', method: 'GET' },
+  {
+    name: 'POST /api/users/echo (body + validation)',
+    path: '/api/users/echo',
+    method: 'POST',
+    body: JSON.stringify({ name: 'Bench User', email: 'bench@nofa.dev', age: 30 }),
+  },
+];
+
+async function main() {
+  console.log(
+    `[bench] duration=${opts.duration}s connections=${opts.connections} warmup=${opts.warmup}s rounds=${opts.rounds} node=${process.version} platform=${process.platform}/${process.arch}`,
+  );
+
+  const micro = await routeMicroBench();
+
+  const baseline = await startServer('baseline', 'baseline-server.mjs', here);
+  const nofault = await startServer('nofault', 'dist/main.js', join(repoRoot, 'examples/v0.2.0-rest-user-api'));
+
+  const results = {};
+  try {
+    for (const sc of SCENARIOS) {
+      const baseRuns = [];
+      const nfRuns = [];
+      for (let r = 1; r <= opts.rounds; r++) {
+        process.stdout.write(`[bench] ${sc.name} round ${r}/${opts.rounds} ... `);
+        const b = await run(baseline.port, sc, opts);
+        const n = await run(nofault.port, sc, opts);
+        baseRuns.push(b);
+        nfRuns.push(n);
+        console.log(`baseline=${b.rps} rps, nofault=${n.rps} rps`);
+      }
+      const base = pickMedianRound(baseRuns);
+      const nf = pickMedianRound(nfRuns);
+      const gap = (((base.rps - nf.rps) / base.rps) * 100).toFixed(1);
+      results[sc.name] = { baseline: base, nofault: nf, gapPercent: Number(gap) };
+      console.table({
+        [`baseline · ${sc.name}`]: { rps: base.rps, mean: base.meanMs, p50: base.p50Ms, p95: base.p95Ms, p99: base.p99Ms },
+        [`nofault · ${sc.name}`]: { rps: nf.rps, mean: nf.meanMs, p50: nf.p50Ms, p95: nf.p95Ms, p99: nf.p99Ms },
+      });
+      console.log(`[bench] gap for "${sc.name}": ${gap}% (正数表示 nofault 更慢)`);
+    }
+  } finally {
+    nofault.child.kill('SIGTERM');
+    baseline.child.kill('SIGTERM');
+  }
+
+  if (opts.report) {
+    writeFileSync(join(here, 'results.json'), JSON.stringify({ opts, micro, results }, null, 2));
+    console.log('[bench] wrote results.json');
+  }
+}
+
+await main();
