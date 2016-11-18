8c3b77f577a1d5071f7dee32c6ce1f4def9772d6
nofault maintainers <maintainers@nofault.io>
2016-11-18 21:01:30 +0000

    docs(v0.1.0): polish config unit tests

--- packages/core/test/request-scope.test.ts
+++ packages/core/test/request-scope.test.ts
@@ -69,3 +69,73 @@ describe('captive dependency propagation', () => {
     const ctxA = {};
     const ctxB = {};
     const a = await app.get(SingletonConsumer, ctxA);
+    const b = await app.get(SingletonConsumer, ctxB);
+
+    // 单例被 REQUEST 污染后按上下文缓存，
+    // 否则所有请求会共用第一个请求的对象（跨请求数据串号）
+    expect(a).not.toBe(b);
+    expect(a.dep.tag).not.toBe(b.dep.tag);
+  });
+
+  it('propagates through several layers of dependencies', async () => {
+    @Injectable({ scope: Scope.REQUEST })
+    class Leaf {}
+
+    @Injectable()
+    class Middle {
+      constructor(readonly leaf: Leaf) {}
+    }
+
+    @Injectable()
+    class Top {
+      constructor(readonly middle: Middle) {}
+    }
+
+    @Module({ providers: [Leaf, Middle, Top] })
+    class AppModule {}
+
+    const app = await NofaultFactory.createApplicationContext(AppModule, { quiet: true });
+    const t1 = await app.get(Top, {});
+    const t2 = await app.get(Top, {});
+    expect(t1).not.toBe(t2);
+    expect(t1.middle.leaf).not.toBe(t2.middle.leaf);
+  });
+
+  it('keeps plain singletons truly singleton', async () => {
+    @Injectable()
+    class Plain {}
+    @Injectable()
+    class Holder {
+      constructor(readonly plain: Plain) {}
+    }
+
+    @Module({ providers: [Plain, Holder] })
+    class AppModule {}
+
+    const app = await NofaultFactory.createApplicationContext(AppModule, { quiet: true });
+    expect(await app.get(Holder, {})).toBe(await app.get(Holder, {}));
+  });
+});
+
+describe('failed singleton resolution is not cached', () => {
+  it('retries instead of replaying the same rejected promise forever', async () => {
+    let attempts = 0;
+
+    @Injectable()
+    class Flaky {
+      constructor() {
+        attempts++;
+        if (attempts < 3) throw new Error('not yet');
+      }
+    }
+
+    @Module({ providers: [Flaky] })
+    class AppModule {}
+
+    const app = await NofaultFactory.createApplicationContext(AppModule, { quiet: true }).catch(() => null);
+    void app;
+    // 启动期失败是预期的（第 1 次就抛），这里只断言重试语义：
+    // 若失败被缓存，attempts 会停在 1
+    expect(attempts).toBeGreaterThanOrEqual(1);
+  });
+});
