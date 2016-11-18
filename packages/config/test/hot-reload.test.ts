b2f106679a01145befc9332a4273e8bbae33a8e9
nofault maintainers <maintainers@nofault.io>
2016-11-18 18:22:14 +0000

    docs(v0.1.0): polish hand-written router

--- packages/config/test/hot-reload.test.ts
+++ packages/config/test/hot-reload.test.ts
@@ -60,3 +60,65 @@ describe('ConfigRegistry', () => {
   it('reload replaces the whole object so stale references never see torn state', async () => {
     let v = 1;
     const registry = new ConfigRegistry({ sources: [{ name: 'live', load: () => ({ v }) }], envPrefix: '' });
+    await registry.start();
+    const before = registry.values();
+    v = 2;
+    await registry.reload();
+    expect(before.v).toBe(1);
+    expect(registry.values().v).toBe(2);
+  });
+
+  it('notifies subscribers only when something actually changed', async () => {
+    const registry = new ConfigRegistry({ sources: [createInlineSource({ v: 1 })], envPrefix: '' });
+    await registry.start();
+    const listener = vi.fn();
+    const off = registry.subscribe(listener);
+
+    await registry.reload();
+    expect(listener).not.toHaveBeenCalled();
+
+    // 换一个会变的源
+    const mutable = createInlineSource({ v: 1 });
+    let counter = 1;
+    const live = { name: 'live', load: () => ({ v: ++counter }) };
+    const registry2 = new ConfigRegistry({ sources: [mutable, live], envPrefix: '' });
+    await registry2.start();
+    const listener2 = vi.fn();
+    registry2.subscribe(listener2);
+    await registry2.reload();
+    expect(listener2).toHaveBeenCalledTimes(1);
+
+    off();
+    expect(registry.listenerCount).toBe(0);
+    await registry.close();
+  });
+});
+
+describe('ConfigService over a registry', () => {
+  it('reads the live snapshot and reports reloadability', async () => {
+    let value = 'first';
+    const registry = new ConfigRegistry({
+      sources: [{ name: 'live', load: () => ({ greeting: value }) }],
+      envPrefix: '',
+    });
+    await registry.start();
+    const config = createConfigService(registry);
+
+    expect(config.get('greeting')).toBe('first');
+    expect(config.isReloadable).toBe(true);
+
+    value = 'second';
+    await config.reload();
+    expect(config.get('greeting')).toBe('second');
+  });
+
+  it('static config is not reloadable', () => {
+    const config = createConfigService({ a: 1 });
+    expect(config.isReloadable).toBe(false);
+    expect(config.subscribe(() => undefined)).toBeTypeOf('function');
+  });
+});
+
+describe('createPollingSource', () => {
+  it('keeps the last known value when fetch fails', async () => {
+    let shouldFail = false;
