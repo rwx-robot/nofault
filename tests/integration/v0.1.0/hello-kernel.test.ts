870fd41ab1e9cf2fd6d7e164edfa39df25a4c38c
Ny <johnnynode@gmail.com>
2016-11-18 15:20:29 +0000

    docs(v0.1.0): document benchmark harness

--- tests/integration/v0.1.0/hello-kernel.test.ts
+++ tests/integration/v0.1.0/hello-kernel.test.ts
@@ -32,3 +32,35 @@ describe('v0.1.0 hello-kernel (integration)', () => {
     expect(res.status).toBe(200);
     expect(await res.text()).toContain('nofault v0.1.0');
   });
+
+  it('GET /health returns ok with the app name from config', async () => {
+    const res = await fetch(`${baseUrl}/health`);
+    expect(res.status).toBe(200);
+    const body = (await res.json()) as { status: string; app: string };
+    expect(body.status).toBe('ok');
+    expect(body.app).toBe('hello-kernel');
+  });
+
+  it('GET /hello?name= greets the caller using the configured greeting', async () => {
+    const res = await fetch(`${baseUrl}/hello?name=world`);
+    expect(res.status).toBe(200);
+    const body = (await res.json()) as { message: string; app: string };
+    expect(body.message).toBe('Hello from nofault, world!');
+    expect(body.app).toBe('hello-kernel');
+  });
+
+  it('GET /hello without name falls back to a bare greeting', async () => {
+    const res = await fetch(`${baseUrl}/hello`);
+    const body = (await res.json()) as { message: string };
+    expect(body.message).toBe('Hello from nofault!');
+  });
+
+  it('unknown routes return 404', async () => {
+    const res = await fetch(`${baseUrl}/does-not-exist`);
+    expect(res.status).toBe(404);
+  });
+
+  it('closes gracefully and stops listening', async () => {
+    expect(app.isListening).toBe(true);
+  });
+});
