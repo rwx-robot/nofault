8c9d29a9de3c04f43cc43357e5ee7bb2fe17e87f
Ny <johnnynode@gmail.com>
2017-03-06 11:39:45 +0000

    feat(rest): add route metadata storage

--- packages/rest/src/decorators.ts
+++ packages/rest/src/decorators.ts
@@ -17,3 +17,10 @@ export interface ControllerOptions {
   /** 控制器级中间件（类或函数） */
   middleware?: Array<unknown>;
 }
+
+export function Controller(options: string | ControllerOptions = '/'): ClassDecorator {
+  return (target) => {
+    const opts = typeof options === 'string' ? { path: options } : options;
+    Reflect.defineMetadata(REST_METADATA.CONTROLLER_PATH, opts.path ?? '/', target);
+    if (opts.middleware?.length) {
+      Reflect.defineMetadata(REST_METADATA.CONTROLLER_MIDDLEWARE, opts.middleware, target);
