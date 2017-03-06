8eb40835915b5dc5c98b46a3894d0d8b4cb132e4
Ny <johnnynode@gmail.com>
2017-03-06 11:11:56 +0000

    feat(rest): add http code decorator

--- packages/rest/src/decorators.ts
+++ packages/rest/src/decorators.ts
@@ -10,3 +10,10 @@ import {
 } from './metadata';
 
 // ------------------------------------------------------------------ 控制器
+
+export interface ControllerOptions {
+  /** 路由前缀 */
+  path?: string;
+  /** 控制器级中间件（类或函数） */
+  middleware?: Array<unknown>;
+}
