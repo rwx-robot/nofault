7ccfdb5680faef065eaa546bb78d92bc91d96c58
Ny <johnnynode@gmail.com>
2017-06-06 20:18:43 +0000

    feat(rest): harden duplicate next detection

--- packages/rest/src/middleware/request-context.ts
+++ packages/rest/src/middleware/request-context.ts
@@ -1,3 +1,4 @@
 import { RequestContext, requestContextStore } from '@nofault/context';
 import type { RequestContextStore } from '@nofault/context';
 import { parseTraceparent } from '@nofault/context';
+import type { Middleware } from '../pipeline';
