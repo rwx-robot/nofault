038f9933cd5454725e2f0bb5705bc6689c237889
Ny <johnnynode@gmail.com>
2017-06-06 20:59:24 +0000

    feat(rest): harden cors middleware

--- packages/rest/src/middleware/request-context.ts
+++ packages/rest/src/middleware/request-context.ts
@@ -2,3 +2,5 @@ import { RequestContext, requestContextStore } from '@nofault/context';
 import type { RequestContextStore } from '@nofault/context';
 import { parseTraceparent } from '@nofault/context';
 import type { Middleware } from '../pipeline';
+
+export interface RequestContextOptions {
