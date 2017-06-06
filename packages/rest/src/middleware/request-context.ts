681e546e2709610de8e549a780293cae8c610c4d
Ny <johnnynode@gmail.com>
2017-06-06 23:04:38 +0000

    feat(rest): harden security headers

--- packages/rest/src/middleware/request-context.ts
+++ packages/rest/src/middleware/request-context.ts
@@ -6,3 +6,5 @@ import type { Middleware } from '../pipeline';
 export interface RequestContextOptions {
   /** 自定义 store；默认用全局的 `requestContextStore` */
   store?: RequestContextStore;
+  /** 是否把 requestId 写进响应头，默认 true */
+  exposeRequestId?: boolean;
