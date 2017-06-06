c865494b692505926667767dc3c4d35d970aeca1
Ny <johnnynode@gmail.com>
2017-06-06 19:48:41 +0000

    feat(rest): harden compose function

--- packages/rest/src/middleware/request-context.ts
+++ packages/rest/src/middleware/request-context.ts
@@ -1,2 +1,3 @@
 import { RequestContext, requestContextStore } from '@nofault/context';
 import type { RequestContextStore } from '@nofault/context';
+import { parseTraceparent } from '@nofault/context';
