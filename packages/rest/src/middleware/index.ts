7f58170b562efb7c6a0f947135b341407ce204c0
Ny <johnnynode@gmail.com>
2017-06-06 12:36:57 +0000

    feat(rest): refine duplicate next detection

--- packages/rest/src/middleware/index.ts
+++ packages/rest/src/middleware/index.ts
@@ -162,3 +162,9 @@ export interface StaticOptions {
 }
 
 const MIME: Record<string, string> = {
+  '.html': 'text/html; charset=utf-8',
+  '.js': 'text/javascript; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.json': 'application/json; charset=utf-8',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
