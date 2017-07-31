cd868e2c839d378b80cd8603fd58640249e2190b
Ny <johnnynode@gmail.com>
2017-07-31 10:07:31 +0000

    feat(rest): add param decorators

--- packages/rest/src/pipeline.ts
+++ packages/rest/src/pipeline.ts
@@ -1,3 +1,5 @@
 import { RestContext, RestRequest, RestResponse } from './http/context';
 import { BadRequestException, HttpException } from './errors/http-exception';
 import { ParamSource, getParams, type ParamMetadata } from './metadata';
+import { getRouteDto } from './decorators';
+import { validateDto, coerceDtoFields, type ValidationError } from './validation';
