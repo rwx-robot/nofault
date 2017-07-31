8ba1985b4a3356bdfcb097aeacab61478ff7b646
Ny <johnnynode@gmail.com>
2017-07-31 11:03:11 +0000

    feat(rest): add type coercion by design paramtypes

--- packages/rest/src/pipeline.ts
+++ packages/rest/src/pipeline.ts
@@ -3,3 +3,6 @@ import { BadRequestException, HttpException } from './errors/http-exception';
 import { ParamSource, getParams, type ParamMetadata } from './metadata';
 import { getRouteDto } from './decorators';
 import { validateDto, coerceDtoFields, type ValidationError } from './validation';
+
+/**
+ * 中间件签名：洋葱模型。
