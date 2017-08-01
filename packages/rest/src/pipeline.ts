8f11fbe8b3df837068cbadc3d44fc9eb855aac16
Ny <johnnynode@gmail.com>
2017-08-01 20:16:49 +0000

    feat(rest): test default values

--- packages/rest/src/pipeline.ts
+++ packages/rest/src/pipeline.ts
@@ -89,3 +89,5 @@ type RouteLike = { propertyKey: string | symbol };
  *
  * `Reflect.getMetadata` + `sort()` 是**每请求**都要走的路径，
  * 在热路径上反复做反射查询开销可观（实测占 POST 场景不小比例），
+ * 因此按 (target, propertyKey) 记忆化一次。
+ */
