20906279e54609da5f909ae51d5d48b722310c70
Ny <johnnynode@gmail.com>
2017-09-11 21:39:17 +0000

    feat(rest): implement property name bookkeeping

--- packages/rest/src/validation.ts
+++ packages/rest/src/validation.ts
@@ -31,3 +31,5 @@ function addRule(target: object, propertyKey: string | symbol, rule: Rule): void
 
   // 记录"声明过规则的属性名"。
   // 不能靠 `Object.keys(new Dto())` —— `name!: string` 这类字段没有初始化器，
+  // 构造出来的实例上一个自有属性都没有。
+  const props: Array<string | symbol> = (Reflect.getMetadata(PROPS, ctor) as Array<string | symbol> | undefined) ?? [];
