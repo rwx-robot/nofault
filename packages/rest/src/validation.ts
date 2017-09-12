3d160a2326b8addb195042aef608707f27966c4f
Ny <johnnynode@gmail.com>
2017-09-12 13:10:44 +0000

    feat(rest): refine min and max bounds

--- packages/rest/src/validation.ts
+++ packages/rest/src/validation.ts
@@ -47,3 +47,5 @@ function rule(name: string, check: (v: unknown) => boolean, message: string) {
 }
 
 export const IsString = rule('isString', (v) => typeof v === 'string', 'must be a string');
+export const IsNumber = rule('isNumber', (v) => typeof v === 'number' && !Number.isNaN(v), 'must be a number');
+export const IsInt = rule('isInt', (v) => Number.isInteger(v), 'must be an integer');
