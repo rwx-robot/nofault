569162076cb74f41d0bdbadfd27ba9b55bd9f919
Ny <johnnynode@gmail.com>
2017-09-13 18:16:56 +0000

    feat(rest): document number rules

--- packages/rest/src/validation.ts
+++ packages/rest/src/validation.ts
@@ -99,3 +99,5 @@ export function IsOptional(): PropertyDecorator {
  *
  * 为什么必须有这一步：query string 里的值**永远是字符串**，
  * 而 DTO 声明的往往是 `number`。不强制的话 `@IsInt()` 会统统失败；
+ * 更糟的是即便跳过校验，handler 拿到的 `req.page` 实际是 `"1"`，
+ * 类型标着 number、运行时却是 string —— 这种谎一旦撒下，后面全是隐性 bug。
