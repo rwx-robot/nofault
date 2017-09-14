a46a7a8360aa5185d50340e341c62aaa726d93f1
Ny <johnnynode@gmail.com>
2017-09-14 16:14:56 +0000

    feat(rest): polish validate body integration

--- packages/rest/src/validation.ts
+++ packages/rest/src/validation.ts
@@ -131,3 +131,4 @@ export function validateDto(dtoClass: new () => object, plain: unknown): Validat
   }
   const source = plain as Record<string, unknown>;
   const errors: ValidationError[] = [];
+  const props: Array<string | symbol> = (Reflect.getMetadata(PROPS, dtoClass) as Array<string | symbol> | undefined) ?? [];
