977d83a3f3d6f6c2968f7cd29d89fe3e45601fdf
Ny <johnnynode@gmail.com>
2017-09-14 13:40:13 +0000

    feat(rest): polish optional rule

--- packages/rest/src/validation.ts
+++ packages/rest/src/validation.ts
@@ -126,3 +126,4 @@ export function coerceDtoFields(dtoClass: new () => object, plain: unknown): Rec
 
 /** 校验一个普通对象是否满足 DTO 类上声明的规则 */
 export function validateDto(dtoClass: new () => object, plain: unknown): ValidationError[] {
+  if (plain === null || typeof plain !== 'object') {
