6091035d6e974a6c8b765682ae6f715cfe465b2a
Ny <johnnynode@gmail.com>
2017-09-14 19:33:34 +0000

    feat(rest): fix edge cases in length bounds

--- packages/rest/src/validation.ts
+++ packages/rest/src/validation.ts
@@ -141,3 +141,5 @@ export function validateDto(dtoClass: new () => object, plain: unknown): Validat
     const optional = list.some((r) => r.name === 'optional');
     const value = source[key];
     if ((value === undefined || value === null || value === '') && optional) continue;
+
+    const constraints: Record<string, string> = {};
