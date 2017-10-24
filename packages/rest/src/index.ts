e1fd076e7f4926919bbb3f7bbec547323607f6c4
Ny <johnnynode@gmail.com>
2017-10-24 19:01:14 +0000

    feat(rest): test common exception subclasses

--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -70,3 +70,8 @@ export {
   normalizeError,
 } from './pipeline';
 export type { Middleware, Interceptor } from './pipeline';
+// [history] omitted at this version (not yet introduced): export { openSse } from './sse';
+// [history] omitted at this version (not yet introduced): export type { SseWriter } from './sse';
+export {
+  cors,
+  bodyParser,
