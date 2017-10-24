21dc08a15c93a52d1f58a9376cddfd4d4b77663b
Ny <johnnynode@gmail.com>
2017-10-24 10:29:25 +0000

    feat(rest): refine unified response wrapper

--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -4,3 +4,8 @@
  * 提供：Radix 路由树、装饰器控制器、中间件管道、参数绑定与校验、异常过滤器。
  * 规范：Node.js / NestJS 生态约定（camelCase 导出、createXxx 工厂）。
  */
+import 'reflect-metadata';
+
+export { RouteTree, RouteTable, RouteConflictError } from './router/route-tree';
+export type { RouteMatch } from './router/route-tree';
+export { SegmentType } from './router/route-tree';
