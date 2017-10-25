91b2222908bc430138fee0e4a73b5f5e33b26c66
Ny <johnnynode@gmail.com>
2017-10-25 10:57:36 +0000

    feat(rest): document error normalization

--- packages/rest/src/rest-application.ts
+++ packages/rest/src/rest-application.ts
@@ -28,3 +28,18 @@ export interface RestApplicationOptions {
   /** 全局中间件 */
   middleware?: Middleware[];
   /** 是否把返回值包装成 `{ code, data, message }`，默认 true */
+  wrapResponse?: boolean;
+  /** 自定义日志器 */
+  logger?: Logger;
+  /**
+   * 上下文存储。默认开启（全局 `requestContextStore`）。
+   * 传 `null` 关闭——但没有上下文时 `Scope.REQUEST` 的 Provider 无法解析。
+   */
+  contextStore?: RequestContextStore | null;
+  /**
+   * 健康检查端点。默认开启：`/healthz`（存活）+ `/readyz`（就绪）。
+   * 传 `false` 关闭，或传对象自定义路径。
+   */
+  health?: false | { livenessPath?: string; readinessPath?: string };
+  /**
+   * 命名中间件表：契约/装饰器里以**字符串**声明的中间件在这里登记实现。
