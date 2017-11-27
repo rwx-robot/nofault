a6e717fcc98216eb8f679fa549ea1df70c77e135
Ny <johnnynode@gmail.com>
2017-11-27 10:23:00 +0000

    feat(core): add controller registry on module ref

--- packages/core/src/application/http-adapter.interface.ts
+++ packages/core/src/application/http-adapter.interface.ts
@@ -6,3 +6,11 @@ import type { IncomingMessage, ServerResponse } from 'node:http';
  * 返回 `true` 表示"我已处理完这个请求"，处理链到此结束；
  * 返回 `false`/`undefined` 表示"我不管"，继续交给下一个 handler。
  */
+export type HttpHandler = (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => void | boolean | Promise<void | boolean>;
+
+/**
+ * HTTP 适配器抽象。
+ *
