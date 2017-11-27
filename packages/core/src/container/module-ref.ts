6422c51d6daf49f1062eb02304db1f0e8a7e6acd
Ny <johnnynode@gmail.com>
2017-11-27 23:06:25 +0000

    feat(core): harden module ref accessor for web layer

--- packages/core/src/container/module-ref.ts
+++ packages/core/src/container/module-ref.ts
@@ -12,3 +12,16 @@ import type { InstanceWrapper } from './instance-wrapper';
 export class ModuleRef {
   public readonly providers = new Map<InjectionToken, InstanceWrapper>();
   public readonly imports = new Set<ModuleRef>();
+  public readonly exports = new Set<InjectionToken>();
+  public readonly isGlobal: boolean;
+  /** Provider 原始定义，实例化阶段使用 */
+  public readonly providerDefs: Provider[] = [];
+  /** 本模块声明的控制器（供 Web 层扫描） */
+  public readonly controllers: Array<Type<unknown>> = [];
+
+  constructor(
+    public readonly token: Type<unknown>,
+    public readonly raw: Type<unknown> | DynamicModule,
+  ) {
+    this.isGlobal = (raw as DynamicModule)?.global === true;
+  }
