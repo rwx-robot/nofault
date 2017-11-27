f52d0b9e629af0ed1ac94e3947ee152d0e57f34a
Ny <johnnynode@gmail.com>
2017-11-27 11:01:14 +0000

    feat(core): add scanner controller registration

--- packages/core/src/application/http-adapter.interface.ts
+++ packages/core/src/application/http-adapter.interface.ts
@@ -14,3 +14,11 @@ export type HttpHandler = (
 /**
  * HTTP 适配器抽象。
  *
+ * nofault 内核不绑定任何具体实现：v0.1.0 由 `@nofault/http` 提供 `node:http` 实现，
+ * 后续版本可替换为 Fastify / uWebSockets / Bun 等适配器。
+ */
+export interface HttpAdapter {
+  /** 挂载请求处理函数 */
+  useHandler(handler: HttpHandler): void;
+  /** 开始监听；返回实际监听的地址信息 */
+  listen(port: number, hostname?: string): Promise<{ port: number; hostname: string }>;
