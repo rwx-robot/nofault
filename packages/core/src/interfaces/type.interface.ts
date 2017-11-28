16b031d18c00da6196ca4d4a205edd0aa12adf62
Ny <johnnynode@gmail.com>
2017-11-28 20:25:31 +0000

    feat(core): fix edge cases in controllers field on module metadata

--- packages/core/src/interfaces/type.interface.ts
+++ packages/core/src/interfaces/type.interface.ts
@@ -31,3 +31,19 @@ export interface OnModuleInit {
 }
 
 export interface OnApplicationBootstrap {
+  onApplicationBootstrap(): void | Promise<void>;
+}
+
+export interface OnModuleDestroy {
+  onModuleDestroy(): void | Promise<void>;
+}
+
+export interface BeforeApplicationShutdown {
+  beforeApplicationShutdown(signal?: string): void | Promise<void>;
+}
+
+export interface OnApplicationShutdown {
+  onApplicationShutdown(signal?: string): void | Promise<void>;
+}
+
+export interface NofaultApplicationContext {
