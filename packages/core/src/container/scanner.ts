ddc2c37b33b2d44a40e9ed68280ec76475329a5a
Ny <johnnynode@gmail.com>
2017-11-28 12:17:23 +0000

    feat(core): test module ref accessor for web layer

--- packages/core/src/container/scanner.ts
+++ packages/core/src/container/scanner.ts
@@ -59,3 +59,22 @@ export class ModuleScanner {
 
     return ref;
   }
+
+  /** 打印模块树（调试 / 文档用） */
+  static printTree(ref: ModuleRef, depth = 0, seen = new Set<ModuleRef>()): string {
+    const pad = '  '.repeat(depth);
+    let out = `${pad}${ref.name}\n`;
+    if (seen.has(ref)) return `${pad}${ref.name} (circular)\n`;
+    seen.add(ref);
+    for (const imp of ref.imports) {
+      out += ModuleScanner.printTree(imp, depth + 1, seen);
+    }
+    return out;
+  }
+
+  static describe(ref: ModuleRef): string {
+    const providers = [...ref.providers.keys()].map(tokenToString).join(', ');
+    const exports = [...ref.exports].map(tokenToString).join(', ');
+    return `Module(${ref.name}) providers=[${providers}] exports=[${exports}]`;
+  }
+}
