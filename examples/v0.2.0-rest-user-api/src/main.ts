bd8b394a045cb9b183af5df480507271b672b601
Ny <johnnynode@gmail.com>
2017-12-13 21:46:31 +0000

    docs(v0.2.0): harden changelog

--- examples/v0.2.0-rest-user-api/src/main.ts
+++ examples/v0.2.0-rest-user-api/src/main.ts
@@ -18,24 +18,3 @@ async function bootstrap(): Promise<void> {
       bodyParser({ limit: 512 * 1024 }),
     ],
   });
-
-  app.enableShutdownHooks();
-
-  // 演示用：预置两条数据
-  const users = await app.get(UserService);
-  reset();
-  users.create({ name: 'Ada Lovelace', email: 'ada@nofault.dev', age: 36 });
-  users.create({ name: 'Alan Turing', email: 'alan@nofault.dev', age: 41 });
-
-  const port = Number(process.env.PORT ?? 3000);
-  const { port: actualPort } = await app.listen(port);
-  logger.info('rest api ready', { port: actualPort, routes: app.getRoutes().length });
-  for (const r of app.getRoutes()) {
-    logger.info(`  ${r.method.padEnd(6)} ${r.path}`);
-  }
-}
-
-void bootstrap().catch((err: unknown) => {
-  logger.error('bootstrap failed', undefined, err);
-  process.exit(1);
-});
