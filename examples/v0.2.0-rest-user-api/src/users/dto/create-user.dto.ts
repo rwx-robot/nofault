dd95841f7703918a0af67053c16834ceab8d23d6
Ny <johnnynode@gmail.com>
2017-12-14 09:20:23 +0000

    docs(v0.2.0): harden example readme

--- examples/v0.2.0-rest-user-api/src/users/dto/create-user.dto.ts
+++ examples/v0.2.0-rest-user-api/src/users/dto/create-user.dto.ts
@@ -10,3 +10,14 @@ export class CreateUserDto {
   @IsNotEmpty()
   @MinLength(2)
   @MaxLength(32)
+  name!: string;
+
+  @IsString()
+  @IsEmail()
+  email!: string;
+
+  @IsInt()
+  @Min(0)
+  @Max(150)
+  age!: number;
+}
