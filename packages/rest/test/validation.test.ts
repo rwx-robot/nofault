e8b2443cb983d3d955924b0817acd88938e3896b
Ny <johnnynode@gmail.com>
2017-12-14 15:36:59 +0000

    docs(v0.2.0): test rest integration tests

--- packages/rest/test/validation.test.ts
+++ packages/rest/test/validation.test.ts
@@ -19,3 +19,23 @@ class DemoDto {
 describe('validateDto', () => {
   it('passes a valid object', () => {
     expect(validateDto(DemoDto, { name: 'Ada', email: 'ada@x.dev', age: 36 })).toEqual([]);
+  });
+
+  it('reports every failing constraint', () => {
+    const errors = validateDto(DemoDto, { name: 'X', email: 'bad', age: 999 });
+    expect(errors).toHaveLength(3);
+    const byProp = Object.fromEntries(errors.map((e) => [e.property, Object.keys(e.constraints)]));
+    expect(byProp.name).toContain('minLength');
+    expect(byProp.email).toContain('isEmail');
+    expect(byProp.age).toContain('max');
+  });
+
+  it('reports missing fields', () => {
+    const errors = validateDto(DemoDto, {});
+    expect(errors.map((e) => e.property).sort()).toEqual(['age', 'email', 'name']);
+  });
+
+  it('rejects non-object bodies', () => {
+    expect(validateDto(DemoDto, null)).toHaveLength(1);
+  });
+});
