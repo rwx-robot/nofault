cce793f79f4c9a037946cde4cf293aea2db26297
Ny <johnnynode@gmail.com>
2018-01-08 09:24:45 +0000

    feat(context): add package manifest for @nofault/context

---
+++ packages/context/src/ids.ts
@@ -0,0 +1,109 @@
+/**
+ * 追踪标识生成：兼容 W3C Trace Context（`traceparent`）。
+ *
+ * 格式：`00-<32 hex traceId>-<16 hex spanId>-<2 hex flags>`
+ *
+ * 为什么现在就做：v0.8.0 才上完整链路追踪，但 traceId 必须在**请求入口**就生成好，
+ * 否则日志里永远补不上——日志一旦写出去就是既成事实。
+ */
+
+const HEX = '0123456789abcdef';
+
+function randomHex(length: number): string {
+  const bytes = new Uint8Array(length / 2);
+  // 用 crypto 而不是 Math.random：traceId 需要足够随机且不阻塞
+  const cryptoObj = globalThis.crypto;
+  if (cryptoObj?.getRandomValues) {
+    cryptoObj.getRandomValues(bytes);
+  } else {
+    for (let i = 0; i < bytes.length; i++) bytes[i] = Math.floor(Math.random() * 256);
+  }
+  let out = '';
+  for (const b of bytes) {
+    out += HEX[(b >> 4) & 0xf];
+    out += HEX[b & 0xf];
+  }
+  return out;
+}
+
+/** 32 位十六进制 traceId */
+export function generateTraceId(): string {
+  return randomHex(32);
+}
+
+/** 16 位十六进制 spanId */
+export function generateSpanId(): string {
+  return randomHex(16);
+}
+
+/** 短请求 ID（日志里人读友好），如 `req_9f3c1a2b` */
+export function generateRequestId(): string {
+  return `req_${randomHex(8)}`;
+}
+
+export interface RequestIds {
+  requestId: string;
+  traceId: string;
+  spanId: string;
+}
+
+/**
+ * 一次性生成请求所需的全部标识。
+ *
+ * 为什么不各自生成：`crypto.getRandomValues` 每次调用都有固定开销，
+ * 建一个请求上下文要 traceId + spanId + requestId 三个，
+ * 分三次调用会让"建立上下文"变成 ~16us —— 在热路径上这是可观的开销。
+ * 一次抽 14 字节再切分，调用次数从 3 降到 1。
+ */
+export function generateRequestIds(): RequestIds {
+  // 32 + 16 + 8 = 56 个 hex 字符 = 28 字节
+  const buf = new Uint8Array(28);
+  const cryptoObj = globalThis.crypto;
+  if (cryptoObj?.getRandomValues) {
+    cryptoObj.getRandomValues(buf);
+  } else {
+    for (let i = 0; i < buf.length; i++) buf[i] = Math.floor(Math.random() * 256);
+  }
+  let hex = '';
+  for (const b of buf) {
+    hex += HEX[(b >> 4) & 0xf];
+    hex += HEX[b & 0xf];
+  }
+  return {
+    traceId: hex.slice(0, 32),
+    spanId: hex.slice(32, 48),
+    requestId: `req_${hex.slice(48, 56)}`,
+  };
+}
+
+export interface TraceParent {
+  traceId: string;
+  spanId: string;
+  /** sampled 标志位 */
+  sampled: boolean;
+}
+
+const TRACEPARENT_RE = /^([0-9a-f]{2})-([0-9a-f]{32})-([0-9a-f]{16})-([0-9a-f]{2})$/;
+
+/**
+ * 解析 `traceparent` 请求头。
+ *
+ * 非法值返回 `undefined` 而不是抛错：
+ * 链路信息是**增强**，不能因为上游给了个坏头就让整个请求失败。
+ */
+export function parseTraceparent(header: string | undefined): TraceParent | undefined {
+  if (!header) return undefined;
+  const m = TRACEPARENT_RE.exec(header.trim().toLowerCase());
+  if (!m) return undefined;
+  const traceId = m[2]!;
+  const spanId = m[3]!;
+  // 全零的 traceId / spanId 视为无效
+  if (/^0+$/.test(traceId) || /^0+$/.test(spanId)) return undefined;
+  const flags = Number.parseInt(m[4]!, 16);
+  return { traceId, spanId, sampled: (flags & 0x01) === 0x01 };
+}
+
+/** 生成 `traceparent` 头，用于向下游传递 */
+export function formatTraceparent(traceId: string, spanId: string, sampled = true): string {
+  return `00-${traceId}-${spanId}-${sampled ? '01' : '00'}`;
+}
---
+++ packages/context/src/request-context.ts
@@ -0,0 +1,92 @@
+import { generateRequestIds, generateSpanId } from './ids';
+import type { TraceParent } from './ids';
+
+export interface RequestContextInit {
+  /** 自定义请求 ID；默认自动生成 */
+  id?: string;
+  /** 复用上游传下来的链路信息 */
+  traceparent?: TraceParent;
+  /** 附加数据 */
+  values?: Record<string, unknown>;
+}
+
+/**
+ * 一次请求的上下文。
+ *
+ * 请求级值传递用 Node 惯用法表达：
+ * 显式对象 + `AsyncLocalStorage` 隐式传播，而不是把 ctx 当第一个参数到处传。
+ *
+ * 它同时扮演两个角色：
+ * 1. **请求级数据袋**（`set` / `get`）
+ * 2. **内核 REQUEST 作用域的 contextId**（对象身份即 key）
+ */
+export class RequestContext {
+  /** 人读友好的请求 ID */
+  public readonly id: string;
+  /** W3C traceId（32 hex） */
+  public readonly traceId: string;
+  /** W3C spanId（16 hex） */
+  public readonly spanId: string;
+  /** 上游传下来的父 spanId */
+  public readonly parentSpanId?: string;
+  /** 是否采样（影响日志与追踪是否落盘） */
+  public readonly sampled: boolean;
+  /** 创建时间（毫秒，hrtime） */
+  public readonly startedAt: number;
+
+  private readonly values = new Map<string, unknown>();
+
+  constructor(init: RequestContextInit = {}) {
+    if (init.id && init.traceparent) {
+      this.id = init.id;
+      this.traceId = init.traceparent.traceId;
+      this.spanId = generateSpanId();
+    } else {
+      const ids = generateRequestIds();
+      this.id = init.id ?? ids.requestId;
+      this.traceId = init.traceparent?.traceId ?? ids.traceId;
+      this.spanId = ids.spanId;
+    }
+    this.parentSpanId = init.traceparent?.spanId;
+    this.sampled = init.traceparent?.sampled ?? true;
+    this.startedAt = performance.now();
+
+    if (init.values) {
+      for (const [k, v] of Object.entries(init.values)) this.values.set(k, v);
+    }
+  }
+
+  set(key: string, value: unknown): this {
+    this.values.set(key, value);
+    return this;
+  }
+
+  get<T = unknown>(key: string): T | undefined {
+    return this.values.get(key) as T | undefined;
+  }
+
+  has(key: string): boolean {
+    return this.values.has(key);
+  }
+
+  delete(key: string): boolean {
+    return this.values.delete(key);
+  }
+
+  /** 快照（写日志用） */
+  toJSON(): Record<string, unknown> {
+    return {
+      id: this.id,
+      traceId: this.traceId,
+      spanId: this.spanId,
+      parentSpanId: this.parentSpanId,
+      sampled: this.sampled,
+      elapsedMs: Number(this.elapsedMs().toFixed(3)),
+    };
+  }
+
+  /** 从创建到现在经过的毫秒数 */
+  elapsedMs(): number {
+    return performance.now() - this.startedAt;
+  }
+}
