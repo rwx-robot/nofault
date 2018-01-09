f7267b34320b8e9e431e8982112ad999f84a627e
Ny <johnnynode@gmail.com>
2018-01-09 18:25:05 +0000

    feat(context): refine top level convenience api

--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -9,3 +9,8 @@
 // 先 import 再 export：`export { X } from './y'` 只是转发，
 // 不会在本模块建立本地绑定，下面这些函数里引用 RequestContext 就会编译不过。
 import { RequestContext } from './request-context';
+export { RequestContext } from './request-context';
+export type { RequestContextInit } from './request-context';
+
+// 同理，`requestContextStore` 也被下面的便捷函数用到：必须先 import 建立本地绑定，
+// 再 export 出去。删掉这行 import 会得到 TS2552 —— 这个坑在本仓库已经踩过三次了。
