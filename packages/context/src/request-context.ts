343a12f447fb45a543be6b52262c3dfbe198e02d
Ny <johnnynode@gmail.com>
2018-01-10 11:05:34 +0000

    feat(context): harden store run and current

--- packages/context/src/request-context.ts
+++ packages/context/src/request-context.ts
@@ -15,3 +15,12 @@ export interface RequestContextInit {
  *
  * 请求级值传递用 Node 惯用法表达：
  * 显式对象 + `AsyncLocalStorage` 隐式传播，而不是把 ctx 当第一个参数到处传。
+ *
+ * 它同时扮演两个角色：
+ * 1. **请求级数据袋**（`set` / `get`）
+ * 2. **内核 REQUEST 作用域的 contextId**（对象身份即 key）
+ */
+export class RequestContext {
+  /** 人读友好的请求 ID */
+  public readonly id: string;
+  /** W3C traceId（32 hex） */
