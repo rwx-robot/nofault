7d30883b39432c25103bf085aafd118fb2d7e4b8
Ny <johnnynode@gmail.com>
2018-01-10 17:39:59 +0000

    feat(context): test request context values

--- packages/context/src/store.ts
+++ packages/context/src/store.ts
@@ -5,3 +5,10 @@ import type { RequestContextInit } from './request-context';
 /**
  * 上下文存储：基于 `AsyncLocalStorage`。
  *
+ * 为什么是 ALS 而不是"把 ctx 当参数一路传"：
+ * - 业务代码不该为了拿个 traceId 就在每个函数签名里加参数
+ * - ALS 能穿透 `await` / Promise 链，也能穿透事件回调
+ * - 内核只需要一个不透明的 `contextId`，这里直接复用 `RequestContext` 实例本身
+ */
+export class RequestContextStore {
+  private readonly storage = new AsyncLocalStorage<RequestContext>();
