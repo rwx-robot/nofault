9fd4a2971d072ec1587e85fb5c7ce325f0b66936
Ny <johnnynode@gmail.com>
2018-01-11 10:18:43 +0000

    feat(context): document request context class

--- packages/context/test/request-context.test.ts
+++ packages/context/test/request-context.test.ts
@@ -11,3 +11,15 @@ import {
   requestContextStore,
   runWithContext,
   setContextValue,
+  getContextValue,
+} from '../src/index';
+
+describe('ids', () => {
+  it('generates w3c-shaped ids', () => {
+    expect(generateTraceId()).toMatch(/^[0-9a-f]{32}$/);
+    expect(generateSpanId()).toMatch(/^[0-9a-f]{16}$/);
+  });
+
+  it('parses a valid traceparent header', () => {
+    const tp = parseTraceparent('00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01');
+    expect(tp).toEqual({
