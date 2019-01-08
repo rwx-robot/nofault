8969c35b4ec401de1f45f982d0f48134e383492f
Ny <johnnynode@gmail.com>
2019-01-08 19:29:49 +0000

    feat(dsl): refine contract decorators

--- packages/dsl/src/index.ts
+++ packages/dsl/src/index.ts
@@ -4,3 +4,8 @@
  * 契约写成普通 TypeScript 文件（`.api.ts`），用装饰器描述元信息。
  * 这样做的好处是：类型由 TS 编译器保证、IDE 原生支持、无需学新语法。
  * 同时保留 `.api` 文本 DSL 作为可选输入，便于迁移。
+ */
+export {
+  FieldSource,
+  createApiSpec,
+  findType,
