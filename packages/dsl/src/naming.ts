7c532179bc6d7f01c442c8b4a1557a29ba495241
Ny <johnnynode@gmail.com>
2019-01-09 13:06:49 +0000

    feat(dsl): harden singularize and handler naming

--- packages/dsl/src/naming.ts
+++ packages/dsl/src/naming.ts
@@ -4,3 +4,10 @@
  * 代码生成的成败一半在命名：`user_api` → `UserApi`、`/user/list` → `userList`。
  * 这些规则集中在这里，避免每个模板各写一套导致风格飘移。
  */
+
+/** 拆词：支持 `-` `_` 分隔与 camelCase / PascalCase 边界 */
+export function splitWords(input: string): string[] {
+  return input
+    .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2')
+    .split(/[^a-zA-Z0-9]+/)
