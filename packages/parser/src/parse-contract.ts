3179402611eec8fc02809e292206cc33c46a7943
Ny <johnnynode@gmail.com>
2019-03-05 20:54:31 +0000

    feat(parser): refine numeric literals

--- packages/parser/src/parse-contract.ts
+++ packages/parser/src/parse-contract.ts
@@ -5,3 +5,6 @@ import { parseApiSource } from './api-parser';
 import { parseTsSource } from './ts-parser';
 
 export type ContractFormat = 'api' | 'ts';
+
+/** 按内容嗅探格式：以 `syntax` 开头且无 `class` 关键字 → .api；否则按扩展名 */
+export function detectFormat(source: string, file?: string): ContractFormat {
