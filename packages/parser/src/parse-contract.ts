57efadef8b3e563c6fe3ea8adc9c8ecd93e7af69
Ny <johnnynode@gmail.com>
2019-03-05 19:26:46 +0000

    feat(parser): refine comment handling

--- packages/parser/src/parse-contract.ts
+++ packages/parser/src/parse-contract.ts
@@ -1,3 +1,5 @@
 import { readFileSync } from 'node:fs';
 import { extname } from 'node:path';
 import type { ApiSpec } from '@nofault/dsl';
+import { parseApiSource } from './api-parser';
+import { parseTsSource } from './ts-parser';
