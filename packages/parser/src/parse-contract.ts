a9dd11cd7b759d57595bb87e2a1410a2a92b689a
Ny <johnnynode@gmail.com>
2019-03-05 21:56:27 +0000

    feat(parser): refine punctuation set for ts contracts

--- packages/parser/src/parse-contract.ts
+++ packages/parser/src/parse-contract.ts
@@ -8,3 +8,6 @@ export type ContractFormat = 'api' | 'ts';
 
 /** 按内容嗅探格式：以 `syntax` 开头且无 `class` 关键字 → .api；否则按扩展名 */
 export function detectFormat(source: string, file?: string): ContractFormat {
+  if (file) {
+    const ext = extname(file).toLowerCase();
+    if (ext === '.api') return 'api';
