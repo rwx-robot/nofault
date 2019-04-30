2c1cefc9f4a57d97e413130343ece675de152bee
Ny <johnnynode@gmail.com>
2019-04-30 20:16:03 +0000

    feat(parser): refine type text reading

--- packages/parser/test/error-location.test.ts
+++ packages/parser/test/error-location.test.ts
@@ -20,3 +20,5 @@ describe('scanner', () => {
   it('treats `!:` / `?:` as punctuation so TS DTOs scan cleanly', () => {
     const tokens = new Scanner('name!: string;\nother?: number;').scan();
     expect(tokens.some((t) => t.type === TokenType.PUNCT && t.value === '!')).toBe(true);
+    expect(tokens.some((t) => t.type === TokenType.PUNCT && t.value === '?')).toBe(true);
+  });
