ad157067e1e83a2cb87328b300e7a628a3e6d39d
Ny <johnnynode@gmail.com>
2019-05-01 17:42:54 +0000

    feat(parser): harden void normalisation

--- packages/parser/test/ts-parser.test.ts
+++ packages/parser/test/ts-parser.test.ts
@@ -5,3 +5,8 @@ import { FieldSource } from '@nofault/dsl';
 
 const CONTRACT = `
 import { Api, Prefix, Group, Jwt, Timeout, Middleware, Get, Post, Handler } from '@nofault/dsl';
+import { Body, IsString, MinLength, IsEmail, Optional, MaxLength } from '@nofault/dsl';
+
+export class LoginReq {
+  @Body('username') @IsString() @MinLength(3) @MaxLength(32)
+  username!: string;
