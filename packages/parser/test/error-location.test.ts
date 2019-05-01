48936beae8eb66ef218de62e417f024cef1be5f7
Ny <johnnynode@gmail.com>
2019-05-01 16:33:30 +0000

    feat(parser): harden method tail skipping

--- packages/parser/test/error-location.test.ts
+++ packages/parser/test/error-location.test.ts
@@ -51,3 +51,4 @@ describe('parse errors carry a position', () => {
   it('rejects malformed decorator args', () => {
     expect(() => parseTsSource('@Get(\nclass X { y(): void {} }', 'x.api.ts')).toThrow(TsParseError);
   });
+});
