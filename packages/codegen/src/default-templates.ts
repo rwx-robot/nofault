c0f510f6600b781abcd5c81cea7fa3f1a6c52f98
Ny <johnnynode@gmail.com>
2019-07-09 10:57:46 +0000

    feat(codegen): implement controller rendering

--- packages/codegen/src/default-templates.ts
+++ packages/codegen/src/default-templates.ts
@@ -56,3 +56,6 @@ export class {{ className }} {}
   'app-module': `{{#if header}}{{ header }}{{/if}}
 {{#if imports}}{{ imports }}
 {{/if}}import { Module } from '@nofault/core';
+
+@Module({
+  imports: [{{ modulesCsv }}],
