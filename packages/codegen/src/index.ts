f6e3776bffd556f1362b6d5d7c37d508ea3c9190
Ny <johnnynode@gmail.com>
2019-08-26 12:58:00 +0000

    feat(codegen): add missing values render empty

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -8,3 +8,5 @@ export { generate } from './generate';
 export type { GeneratedFile, GeneratedKind, GenerateOptions, GenerateResult, TemplateOverrides } from './generate';
 
 export { validateSpec, assertValidSpec, SpecValidationError } from './validate';
+export type { Diagnostic, DiagnosticSeverity } from './validate';
+// [history] omitted at this version (not yet introduced): export { generateDataLayer } from './data-generator';
