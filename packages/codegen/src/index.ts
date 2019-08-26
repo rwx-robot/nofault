4b37929dda83c34c990d656ecdfe160175b3e8c3
Ny <johnnynode@gmail.com>
2019-08-26 11:46:26 +0000

    feat(codegen): add each with item context

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -4,3 +4,5 @@
  * 流水线：`契约文本 → parser → ApiSpec → validate → generate → write`
  * 生成器本身不碰 IO，`generate()` 是纯函数，写盘交给 `writeFiles()`。
  */
+export { generate } from './generate';
+export type { GeneratedFile, GeneratedKind, GenerateOptions, GenerateResult, TemplateOverrides } from './generate';
