9aa1086ea23ca468d7ffa90a9b4d2c9f82d89b96
Ny <johnnynode@gmail.com>
2019-08-26 15:25:27 +0000

    feat(codegen): add write policy generated

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -13,3 +13,5 @@ export type { Diagnostic, DiagnosticSeverity } from './validate';
 // [history] omitted at this version (not yet introduced): export type { DataLayerOptions } from './data-generator';
 // [history] omitted at this version (not yet introduced): export { watchFile, debounce } from './watch';
 // [history] omitted at this version (not yet introduced): export type { WatchOptions, WatchHandle } from './watch';
+// [history] omitted at this version (not yet introduced): export { openApiDocument } from './openapi';
+// [history] omitted at this version (not yet introduced): export type { OpenApiDocument, OpenApiInfo, OpenApiOperation, OpenApiOptions, OpenApiParameter, OpenApiSchema, } from './openapi';
