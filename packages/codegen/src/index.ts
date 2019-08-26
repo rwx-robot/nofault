f412dfdf461d220ce9cba682b77c63032571ab21
Ny <johnnynode@gmail.com>
2019-08-26 14:43:47 +0000

    feat(codegen): add default template set

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -11,3 +11,5 @@ export { validateSpec, assertValidSpec, SpecValidationError } from './validate';
 export type { Diagnostic, DiagnosticSeverity } from './validate';
 // [history] omitted at this version (not yet introduced): export { generateDataLayer } from './data-generator';
 // [history] omitted at this version (not yet introduced): export type { DataLayerOptions } from './data-generator';
+// [history] omitted at this version (not yet introduced): export { watchFile, debounce } from './watch';
+// [history] omitted at this version (not yet introduced): export type { WatchOptions, WatchHandle } from './watch';
