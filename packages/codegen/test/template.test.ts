ce8ba9bc6c680d015b009a81be552e5fd47f6cbc
Ny <johnnynode@gmail.com>
2019-08-28 10:26:30 +0000

    feat(codegen): harden if and unless blocks

--- packages/codegen/test/template.test.ts
+++ packages/codegen/test/template.test.ts
@@ -5,3 +5,8 @@ describe('renderTemplate', () => {
   it('interpolates plain values', () => {
     expect(renderTemplate('hi {{ name }}!', { name: 'bob' })).toBe('hi bob!');
   });
+
+  it('resolves dotted paths', () => {
+    const ctx = { user: { profile: { age: 3 } } };
+    expect(renderTemplate('{{ user.profile.age }}', ctx)).toBe('3');
+  });
