666f7f31fdf1cd31db788cdcfb62da5cbc4f873e
Ny <johnnynode@gmail.com>
2019-08-29 09:33:55 +0000

    feat(codegen): test write policy generated

--- packages/codegen/test/writer.test.ts
+++ packages/codegen/test/writer.test.ts
@@ -38,3 +38,9 @@ describe('writeFiles', () => {
     const r = writeFiles([file('a.ts', `// ${MARKER}\nnew\n`)], { outDir: dir });
     expect(r.written).toEqual(['a.ts']);
     expect(readFileSync(join(dir, 'a.ts'), 'utf8')).toContain('new');
+  });
+
+  it('never clobbers hand-edited files under the default policy', () => {
+    // 这是最该被守住的一条：静默覆盖用户代码是生成器最恶劣的行为
+    const dir = tmp();
+    writeFileSync(join(dir, 'a.ts'), 'const myOwnCode = 1;\n');
