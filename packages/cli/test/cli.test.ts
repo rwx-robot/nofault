344b8e4a954b88ba868a143188e1cbb425b2c753
Ny <johnnynode@gmail.com>
2019-09-30 09:27:06 +0000

    feat(cli): add package manifest and bin

---
+++ packages/cli/test/cli.test.ts
@@ -0,0 +1,159 @@
+import { describe, expect, it } from 'vitest';
+import { mkdtempSync, readFileSync, existsSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { tmpdir } from 'node:os';
+import { run, scaffold, scaffoldMcp, generateApi, VERSION } from '../src/index';
+
+function tmp(): string {
+  return mkdtempSync(join(tmpdir(), 'nfcli-'));
+}
+
+/** 子目录一定不存在 —— scaffold 见到非空目录应当拒绝 */
+function projectPath(name: string): string {
+  return join(tmp(), name);
+}
+
+const CONTRACT = `
+import { Api, Prefix, Get, Post, Handler, Body, Query, IsString, IsInt, MinLength } from '@nofault/dsl';
+
+export class GreetReq {
+  @Body('name') @IsString() @MinLength(1)
+  name!: string;
+}
+
+export class GreetResp {
+  @Body('text') @IsString()
+  text!: string;
+}
+
+export class PageReq {
+  @Query('page') @IsInt()
+  page!: number;
+}
+
+@Api('user')
+@Prefix('/api')
+export class UserService {
+  @Get('/ping')
+  @Handler('ping')
+  ping(): GreetResp { throw new Error('x'); }
+
+  @Post('/greet')
+  greet(req: GreetReq): GreetResp { throw new Error('x'); }
+
+  @Get('/page')
+  page(req: PageReq): GreetResp { throw new Error('x'); }
+}
+`;
+
+describe('scaffold', () => {
+  it('creates a project layout including a sample contract', () => {
+    const dir = projectPath('my-service');
+    const r = scaffold('my-service', { dir });
+    expect(r.files).toContain('src/main.ts');
+    expect(r.files).toContain('src/app.module.ts');
+    expect(r.files).toContain('api/my-service.api.ts');
+    expect(r.files).toContain('package.json');
+    expect(existsSync(join(dir, 'tsconfig.json'))).toBe(true);
+  });
+
+  it('refuses to overwrite an existing directory', () => {
+    const dir = projectPath('my-service');
+    scaffold('my-service', { dir });
+    writeFileSync(join(dir, 'package.json'), 'hand written\n');
+    // 已存在内容时宁可失败：脚手架覆盖用户代码是不可接受的
+    expect(() => scaffold('my-service', { dir })).toThrow(/already exists/);
+  });
+
+  it('kebab-cases the project name everywhere', () => {
+    const dir = projectPath('UserService');
+    scaffold('UserService', { dir });
+    const pkg = JSON.parse(readFileSync(join(dir, 'package.json'), 'utf8'));
+    expect(pkg.name).toBe('user-service');
+    expect(pkg.scripts.generate).toContain('api/user-service.api.ts');
+  });
+});
+
+describe('nofaultctl commands', () => {
+  it('prints help and exits 0 with no args', () => {
+    expect(run([]).exitCode).toBe(0);
+    expect(run(['--help']).exitCode).toBe(0);
+  });
+
+  it('reports its version', () => {
+    expect(VERSION).toMatch(/^\d+\.\d+\.\d+$/);
+  });
+
+  it('returns exit code 1 for unknown commands', () => {
+    expect(run(['wat']).exitCode).toBe(1);
+  });
+
+  it('errors clearly when the contract file does not exist', () => {
+    expect(() => generateApi({ contract: join(tmp(), 'nope.api.ts') })).toThrow(/contract not found/);
+  });
+
+  it('generate writes dto, controller, service and module', () => {
+    const dir = tmp();
+    const contract = join(dir, 'user.api.ts');
+    writeFileSync(contract, CONTRACT, 'utf8');
+
+    const result = generateApi({ contract, out: join(dir, 'src'), rootModule: true });
+    expect(result.spec.services).toHaveLength(1);
+    expect(result.files.map((f) => f.path)).toContain('user/user.controller.ts');
+    expect(result.written.length).toBeGreaterThan(0);
+    expect(existsSync(join(dir, 'src/user/user.service.ts'))).toBe(true);
+    expect(existsSync(join(dir, 'src/app.module.ts'))).toBe(true);
+  });
+
+  it('dry-run generates nothing on disk', () => {
+    const dir = tmp();
+    const contract = join(dir, 'user.api.ts');
+    writeFileSync(contract, CONTRACT, 'utf8');
+
+    generateApi({ contract, out: join(dir, 'src'), dryRun: true });
+    expect(existsSync(join(dir, 'src/user/user.service.ts'))).toBe(false);
+  });
+
+  it('refuses to generate from an invalid contract unless forced', () => {
+    const dir = tmp();
+    const bad = join(dir, 'bad.api.ts');
+    writeFileSync(bad, `@Api('x')\nexport class XService { @Get('no-slash') y(): void {} }\n`, 'utf8');
+
+    expect(() => generateApi({ contract: bad, out: join(dir, 'src') })).toThrow(/refusing to generate/);
+    expect(() => generateApi({ contract: bad, out: join(dir, 'src'), force: true })).not.toThrow();
+  });
+
+  it('scaffold + generate produces a runnable module list', () => {
+    const dir = projectPath('demo');
+    scaffold('demo', { dir });
+    // 脚手架自带的示例契约必须能通过校验，否则"开箱可用"就是空话
+    expect(run(['validate', join(dir, 'api/demo.api.ts')]).exitCode).toBe(0);
+    expect(run(['routes', join(dir, 'api/demo.api.ts')]).exitCode).toBe(0);
+    expect(run(['generate', 'api', join(dir, 'api/demo.api.ts'), '--out', join(dir, 'src'), '--root-module']).exitCode).toBe(0);
+    expect(existsSync(join(dir, 'src/demo/demo.controller.ts'))).toBe(true);
+  });
+
+  it('reports contract errors with a non-zero exit code', () => {
+    const dir = tmp();
+    const bad = join(dir, 'bad.api.ts');
+    writeFileSync(bad, `@Api('x')\nexport class XService { @Get('/a') a(): void {} @Get('/a') b(): void {} }\n`, 'utf8');
+    expect(run(['validate', bad]).exitCode).toBe(1);
+  });
+
+  it('scaffolds a runnable mcp server with a sample tool and wires cli dispatch', () => {
+    const dir = mkdtempSync(join(tmpdir(), 'nfcli-mcp-'));
+    // 函数层：scaffoldMcp 直接验证生成结果
+    const result = scaffoldMcp('echo-server', { dir });
+    const pkg = JSON.parse(readFileSync(join(dir, 'package.json'), 'utf8'));
+    expect(pkg.dependencies).toHaveProperty('@nofault/mcp');
+    expect(pkg.type).toBe('module');
+    expect(result.files).toContain('src/main.ts');
+    expect(result.files).toContain('src/tools.ts');
+
+    // CLI 层：`nofaultctl mcp new` 派发走通
+    expect(run(['mcp', 'new', 'second', '--dir', join(dir, 'second')]).exitCode).toBe(0);
+    expect(existsSync(join(dir, 'second/src/main.ts'))).toBe(true);
+    // 误用应给清晰错误
+    expect(run(['mcp', 'scaffold', 'x']).exitCode).toBe(1);
+  });
+});
---
+++ packages/cli/test/tooling.test.ts
@@ -0,0 +1,181 @@
+/**
+ * v0.10.0 工具链测试。
+ *
+ * 重点在"生成物对不对"而不是"函数有没有抛错"：
+ * - OpenAPI 的路径参数是否转成 `{id}`（写成 `:id` 工具就解析不了）
+ * - 必填字段是否进 required，没有必填时是否**省略**该键（空数组语义相反）
+ * - doctor 能否识别出"装饰器元数据没开"这个最常见的坑
+ */
+import { describe, expect, it } from 'vitest';
+import { mkdtempSync, writeFileSync, mkdirSync } from 'node:fs';
+import { tmpdir } from 'node:os';
+import { join } from 'node:path';
+import { FieldSource, type ApiSpec, type RouteSpec, type ServiceSpec, type TypeSpec } from '@nofault/dsl';
+import { openApiDocument } from '@nofault/codegen';
+import { doctor } from '../src/commands/doctor';
+
+function field(name: string, type: string, source: FieldSource = FieldSource.BODY, optional = false) {
+  return { name, key: name, type, source, optional, rules: [] };
+}
+
+function service(routes: RouteSpec[], overrides: Partial<ServiceSpec> = {}): ServiceSpec {
+  return { name: 'user-api', group: 'user', prefix: '/api', middleware: [], routes, ...overrides };
+}
+
+function spec(services: ServiceSpec[], types: TypeSpec[]): ApiSpec {
+  return { name: 'demo', types, services };
+}
+
+const User: TypeSpec = {
+  name: 'User',
+  fields: [field('id', 'number'), field('name', 'string'), field('bio', 'string', FieldSource.BODY, true)],
+};
+
+const GetUserReq: TypeSpec = {
+  name: 'GetUserReq',
+  fields: [field('id', 'number', FieldSource.PATH), field('verbose', 'boolean', FieldSource.QUERY, true)],
+};
+
+describe('openapi generation', () => {
+  const doc = openApiDocument(
+    spec(
+      [
+        service([
+          { handler: 'getUser', method: 'GET', path: '/users/:id', requestType: 'GetUserReq', responseType: 'User' },
+          { handler: 'createUser', method: 'POST', path: '/users', requestType: 'User', responseType: 'User' },
+          { handler: 'deleteUser', method: 'DELETE', path: '/users/:id', requestType: 'GetUserReq' },
+        ]),
+      ],
+      [User, GetUserReq],
+    ),
+    { title: 'Demo API', version: '2.1.0' },
+  );
+
+  it('renders path params with braces, not colons', () => {
+    // `:id` 在 OpenAPI 里是非法的；Swagger UI / codegen 都会解析失败
+    expect(Object.keys(doc.paths)).toContain('/api/user/users/{id}');
+    expect(Object.keys(doc.paths)).not.toContain('/api/user/users/:id');
+  });
+
+  it('marks path params as required and in:path', () => {
+    const operation = doc.paths['/api/user/users/{id}']!.get!;
+    const id = operation.parameters.find((p) => p.name === 'id');
+    expect(id).toMatchObject({ in: 'path', required: true, schema: { type: 'number' } });
+  });
+
+  it('splits query params out of the body', () => {
+    const operation = doc.paths['/api/user/users/{id}']!.get!;
+    const verbose = operation.parameters.find((p) => p.name === 'verbose');
+    expect(verbose).toMatchObject({ in: 'query', required: false, schema: { type: 'boolean' } });
+    // GET 不该有 requestBody
+    expect(operation.requestBody).toBeUndefined();
+  });
+
+  it('puts required fields in required and omits the key when nothing is required', () => {
+    const user = doc.components.schemas.User!;
+    expect(user.required).toEqual(['id', 'name']);
+    expect(user.properties!).toHaveProperty('bio');
+
+    const optionalOnly: TypeSpec = {
+      name: 'Loose',
+      fields: [field('a', 'string', FieldSource.BODY, true)],
+    };
+    const looseDoc = openApiDocument(spec([], [optionalOnly]));
+    // 空 required 数组在部分工具里会被当成"全部必填"，所以整个键要省掉
+    expect(looseDoc.components.schemas.Loose!.required).toBeUndefined();
+  });
+
+  it('references known types and inlines arrays of them', () => {
+    const listDoc = openApiDocument(
+      spec(
+        [service([{ handler: 'list', method: 'GET', path: '/users', responseType: 'User[]' }])],
+        [User],
+      ),
+    );
+    const schema = listDoc.paths['/api/user/users']!.get!.responses['200']!.content![
+      'application/json'
+    ]!.schema;
+    expect(schema).toEqual({ type: 'array', items: { $ref: '#/components/schemas/User' } });
+  });
+
+  it('uses 204 for void responses and always documents 422', () => {
+    const operation = doc.paths['/api/user/users/{id}']!.delete!;
+    expect(operation.responses['204']).toEqual({ description: 'No Content' });
+    expect(operation.responses['422']).toBeDefined();
+  });
+
+  it('carries the info block through', () => {
+    expect(doc.openapi).toBe('3.0.3');
+    expect(doc.info).toEqual({ title: 'Demo API', version: '2.1.0' });
+  });
+});
+
+describe('doctor', () => {
+  function project(files: Record<string, string>, deps: string[] = []): string {
+    const dir = mkdtempSync(join(tmpdir(), 'nf-doctor-'));
+    for (const [name, content] of Object.entries(files)) {
+      const path = join(dir, name);
+      mkdirSync(join(path, '..'), { recursive: true });
+      writeFileSync(path, content);
+    }
+    for (const dep of deps) mkdirSync(join(dir, 'node_modules', dep), { recursive: true });
+    return dir;
+  }
+
+  it('fails when decorator metadata is off', () => {
+    // 这是本项目最常见的一类"跑不起来"：@Inject / @Column 拿不到类型，
+    // 而报错信息完全指不到 tsconfig
+    const dir = project({
+      'package.json': JSON.stringify({ type: 'module' }),
+      'tsconfig.json': JSON.stringify({ compilerOptions: { target: 'ES2022' } }),
+      'src/main.ts': '',
+    });
+    const report = doctor(dir);
+    const check = report.checks.find((c) => c.name === 'emitDecoratorMetadata');
+    expect(check?.status).toBe('fail');
+    expect(check?.hint).toContain('emitDecoratorMetadata');
+    expect(report.ok).toBe(false);
+  });
+
+  it('passes a correctly configured project', () => {
+    const dir = project({
+      'package.json': JSON.stringify({ type: 'module' }),
+      'tsconfig.json': JSON.stringify({
+        // 故意带注释：tsconfig 允许注释，解析器必须容错
+        compilerOptions: {
+          /* 装饰器元数据 */
+          experimentalDecorators: true,
+          emitDecoratorMetadata: true,
+        },
+      }),
+      'src/main.ts': '',
+    }, ['reflect-metadata']);
+    const report = doctor(dir);
+    expect(report.checks.find((c) => c.name === 'emitDecoratorMetadata')?.status).toBe('ok');
+    expect(report.failures).toBe(0);
+  });
+
+  it('follows tsconfig extends into the base config', () => {
+    // 不解析 extends 会大面积误报：绝大多数工程把装饰器元数据写在基配置里。
+    // 假阴性比漏检更糟 —— 它会让人直接不信任这个工具
+    const dir = project({
+      'package.json': JSON.stringify({ type: 'module' }),
+      'tsconfig.json': JSON.stringify({ extends: './config/tsconfig.base.json' }),
+      'config/tsconfig.base.json': JSON.stringify({
+        compilerOptions: { emitDecoratorMetadata: true, experimentalDecorators: true },
+      }),
+      'src/main.ts': '',
+    }, ['reflect-metadata']);
+
+    const report = doctor(dir);
+    expect(report.checks.find((c) => c.name === 'emitDecoratorMetadata')?.status).toBe('ok');
+    expect(report.failures).toBe(0);
+  });
+
+  it('reports missing reflect-metadata with an actionable hint', () => {
+    const dir = project({ 'package.json': '{}', 'tsconfig.json': '{}' });
+    const check = doctor(dir).checks.find((c) => c.name === 'reflect-metadata');
+    expect(check?.status).toBe('fail');
+    expect(check?.hint).toContain('reflect-metadata');
+  });
+});
