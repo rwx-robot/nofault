e41bec3bd42a4ddf6060e6fba8c098bceda4668c
Ny <johnnynode@gmail.com>
2019-10-01 16:16:14 +0000

    feat(cli): refine argument parser

--- packages/cli/src/commands/generate.ts
+++ packages/cli/src/commands/generate.ts
@@ -24,3 +24,28 @@ export interface GenerateApiOptions {
   /** 模板覆盖目录 */
   templates?: string;
   /** 是否额外生成根模块 */
+  rootModule?: boolean;
+  /** 覆盖策略，默认 `generated` */
+  policy?: WritePolicy;
+  /** 只报告，不写盘 */
+  dryRun?: boolean;
+  /** 额外生成 entity / repository 骨架（数据层） */
+  withOrm?: boolean;
+  /** 监听契约变化并自动重新生成 */
+  watch?: boolean;
+  /** 不合格时也继续生成 */
+  force?: boolean;
+}
+
+export interface GenerateApiResult {
+  spec: ApiSpec;
+  files: GeneratedFile[];
+  written: string[];
+  skipped: string[];
+  changed: string[];
+}
+
+export function compileContract(file: string): ApiSpec {
+  if (!existsSync(file)) {
+    throw new Error(`contract not found: ${file}`);
+  }
