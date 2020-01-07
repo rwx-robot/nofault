747fd32bc850285958f18a04713914231df1a973
Ny <johnnynode@gmail.com>
2020-01-07 19:49:14 +0000

    feat(orm): harden type inference from design type

--- packages/orm/src/decorators.ts
+++ packages/orm/src/decorators.ts
@@ -49,3 +49,12 @@ export interface EntityMeta {
 function toSnakeCase(value: string): string {
   return value
     .replace(/([a-z0-9])([A-Z])/g, '$1_$2')
+    .replace(/[A-Z]+(?![a-z])/g, (m) => `_${m}`)
+    .replace(/^_+/, '')
+    .toLowerCase();
+}
+
+function inferType(designType: unknown): ColumnType {
+  if (designType === Number) return 'int';
+  if (designType === Boolean) return 'boolean';
+  if (designType === Date) return 'date';
