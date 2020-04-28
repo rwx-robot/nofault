d186f946bddce26f068d16b3802da21dbaa57b53
Ny <johnnynode@gmail.com>
2020-04-28 10:24:36 +0000

    feat(orm): implement where grouping

--- packages/orm/src/orm.module.ts
+++ packages/orm/src/orm.module.ts
@@ -3,3 +3,7 @@
  *
  * `OrmModule.forRoot({ dataSource })` 提供全局 DataSource；
  * `OrmModule.forFeature([User])` 为每个实体注册一个 Repository Provider，
+ * token 是 `getRepositoryToken(User)`——注入时用 `@InjectRepository(User)`。
+ *
+ * Repository 是**单例**：它无状态，所有状态都在 DataSource 里。
+ * 这点与请求级服务不同，别把它标成 REQUEST 作用域。
