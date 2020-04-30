cd8c9c6c0f43cb699d8a3f8980da3b9ef6e5f584
Ny <johnnynode@gmail.com>
2020-04-30 11:02:54 +0000

    feat(orm): test query builder

--- packages/orm/test/repository.test.ts
+++ packages/orm/test/repository.test.ts
@@ -22,3 +22,16 @@ class User {
 
   @Column({ name: 'age', type: 'int', nullable: true })
   age?: number;
+
+  @Column({ name: 'email', unique: true })
+  email!: string;
+
+  @Column({ name: 'active', type: 'boolean' })
+  active = true;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt?: Date;
+}
+
+describe('entity mapping', () => {
+  it('derives the table name and column names', () => {
