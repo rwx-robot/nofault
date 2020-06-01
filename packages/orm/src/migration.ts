fcc6dc24600a9b172ff61d59526a09951a171d15
Ny <johnnynode@gmail.com>
2020-06-01 12:28:51 +0000

    feat(orm): add idempotent apply

--- packages/orm/src/migration.ts
+++ packages/orm/src/migration.ts
@@ -5,3 +5,5 @@
  * 1. 每个 migration 只做一件事，且**必须写 down**——没有回滚的迁移等于埋雷
  * 2. 已执行的版本记在 `schema_migrations` 表里，重复执行不会重跑
  * 3. up 失败即中止，后面的版本不执行（顺序是有依赖的）
+ */
+import type { DataSource } from './data-source';
