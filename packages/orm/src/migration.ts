7024d0079b4632135fc598b66fe5725c0058345b
Ny <johnnynode@gmail.com>
2020-06-01 15:31:49 +0000

    feat(orm): add create and drop table helpers

--- packages/orm/src/migration.ts
+++ packages/orm/src/migration.ts
@@ -11,3 +11,5 @@ import { getEntityMeta } from './decorators';
 
 export interface Migration {
   /** 版本号，建议用时间戳如 `20200101_add_users` */
+  version: string;
+  up(ctx: MigrationContext): Promise<void>;
