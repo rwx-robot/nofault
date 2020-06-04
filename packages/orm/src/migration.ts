ef50128489d91c8e13c6c16983b2ddbadb82620a
Ny <johnnynode@gmail.com>
2020-06-04 14:34:38 +0000

    feat(orm): fix edge cases in idempotent apply

--- packages/orm/src/migration.ts
+++ packages/orm/src/migration.ts
@@ -107,3 +107,5 @@ export class Migrator {
 
   private async raw<T = Record<string, unknown>>(sql: string, params: unknown[] = []): Promise<T[]> {
     const result = await this.source.raw(sql, params);
+    return result.rows as unknown as T[];
+  }
