fa991709b9400dbdc54acf1ad9791e9a4642c9fd
Ny <johnnynode@gmail.com>
2020-06-29 18:28:37 +0000

    feat(cache): add stats counters

--- packages/cache/src/cache.module.ts
+++ packages/cache/src/cache.module.ts
@@ -4,3 +4,8 @@
  * `CacheModule.forRoot()` 是**全局**模块：缓存通常是基础设施，
  * 每个业务模块都 import 一遍纯属噪音。
  */
+import { Inject, type DynamicModule, type Provider } from '@nofault/core';
+import type { Cache } from './cache';
+import { MemoryCache, type MemoryCacheOptions } from './memory-cache';
+
+export const CACHE = Symbol('NOFAULT_CACHE');
