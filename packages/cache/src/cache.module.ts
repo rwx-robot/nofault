de184bc19389e7bb931ad01e528f9c0846fff9dc
Ny <johnnynode@gmail.com>
2020-06-29 19:34:46 +0000

    feat(cache): add cache tests

--- packages/cache/src/cache.module.ts
+++ packages/cache/src/cache.module.ts
@@ -13,3 +13,9 @@ export const CACHE = Symbol('NOFAULT_CACHE');
 export function InjectCache(): ParameterDecorator {
   return Inject(CACHE as never);
 }
+
+export interface CacheModuleOptions {
+  /** 直接给一个实现（比如接 Redis 的适配器） */
+  cache?: Cache;
+  /** 或者给参数，由框架造一个 MemoryCache */
+  memory?: MemoryCacheOptions;
