7d3af944aadd726c1a501991a39d83b32d4b3bc7
Ny <johnnynode@gmail.com>
2020-06-30 11:40:29 +0000

    feat(cache): implement get or set

--- packages/cache/src/cache.ts
+++ packages/cache/src/cache.ts
@@ -4,3 +4,9 @@
  * 接口刻意只有 6 个方法，且 `getOrSet` 是**唯一推荐**的用法：
  * 手写 `get → 未命中 → 查库 → set` 这三步，在并发下必然踩坑，
  * 把它收进实现里，业务代码就写不出错的写法。
+ */
+export interface CacheStats {
+  hits: number;
+  misses: number;
+  sets: number;
+  deletes: number;
