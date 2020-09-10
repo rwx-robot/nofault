e57b82e2c2cdb96528020d3832529866b5dde574
Ny <johnnynode@gmail.com>
2020-09-10 22:25:20 +0000

    docs(v0.5.0): document generated sources

--- examples/v0.5.0-data-user-api/src/user/user.controller.ts
+++ examples/v0.5.0-data-user-api/src/user/user.controller.ts
@@ -31,3 +31,10 @@ export class UserController {
   async createUser(@Body() createUserReq: CreateUserReq): Promise<UserResp> {
     return this.service.createUser(createUserReq);
   }
+
+  @Delete('/users/:id')
+  async deleteUser(@Param('id') id: number): Promise<OkResp> {
+    return this.service.deleteUser({ id });
+  }
+
+}
--- examples/v0.5.0-data-user-api/src/user/user.module.ts
+++ examples/v0.5.0-data-user-api/src/user/user.module.ts
@@ -3,17 +3,3 @@
  *
  * 手工加的是 `imports: [DataModule]`：service 依赖 Repository，
  * 而 Repository 由数据层模块提供。生成器不知道你有哪些基础设施模块，
- * 这一步**注定**要人来做——所以模块文件通常是第一批被接管的生成物之一。
- */
-import { Module } from '@nofault/core';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
-import { DataModule } from '../data/data.module';
-
-@Module({
-  imports: [DataModule],
-  controllers: [UserController],
-  providers: [UserService],
-  exports: [UserService],
-})
-export class UserModule {}
