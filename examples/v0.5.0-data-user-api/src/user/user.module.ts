4ae37433e40ff1dd01e59c7c6ef55c49549be04a
Ny <johnnynode@gmail.com>
2020-09-10 23:21:37 +0000

    docs(v0.5.0): document handwritten service with cache

--- examples/v0.5.0-data-user-api/src/user/user.module.ts
+++ examples/v0.5.0-data-user-api/src/user/user.module.ts
@@ -3,3 +3,8 @@
  *
  * 手工加的是 `imports: [DataModule]`：service 依赖 Repository，
  * 而 Repository 由数据层模块提供。生成器不知道你有哪些基础设施模块，
+ * 这一步**注定**要人来做——所以模块文件通常是第一批被接管的生成物之一。
+ */
+import { Module } from '@nofault/core';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
