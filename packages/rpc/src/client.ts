0d2bcd546b2c5a763dc9017b1f8beedf4eb1e3f7
Ny <johnnynode@gmail.com>
2021-04-27 23:05:23 +0000

    feat(rpc): test request id pairing

--- packages/rpc/src/client.ts
+++ packages/rpc/src/client.ts
@@ -119,3 +119,6 @@ export class RpcClient {
       const request: RpcRequest = { id, service, method, payload, traceId };
       connection.socket.write(this.codec.encodeRequest(request), (err) => {
         if (err) {
+          clearTimeout(timer);
+          connection.pending.delete(id);
+          this.discard(connection);
