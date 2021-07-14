256ecb223849f025711b15230d6cca424289324f
Ny <johnnynode@gmail.com>
2021-07-14 19:51:39 +0000

    docs(v0.6.0): test end to end gateway test

--- examples/v0.6.0-rpc-gateway/src/main.ts
+++ examples/v0.6.0-rpc-gateway/src/main.ts
@@ -7,3 +7,12 @@ import { UserRpcService } from './user-rpc.service';
 import { GatewayController } from './gateway.controller';
 
 const logger = createLogger({ context: 'rpc-gateway', level: LogLevel.INFO });
+
+/**
+ * 注册中心必须是**模块级单例**：服务端与客户端看到同一份，
+ * 否则注册了也发现不到。真实环境它是独立进程，这里为了单进程演示放在内存。
+ *
+ * 注意它要定义在 GatewayModule **之前**：装饰器在类定义时就会求值，
+ * 放在后面会踩暂时性死区（TDZ）。
+ */
+const registry = new InMemoryRegistry({ ttlMs: 30_000 });
