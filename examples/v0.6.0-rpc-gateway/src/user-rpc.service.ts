eba40ca45aae1d28dedbee6a8094f505ff6bbf92
Ny <johnnynode@gmail.com>
2021-07-15 10:25:47 +0000

    docs(v0.6.0): test lint cleanups

--- examples/v0.6.0-rpc-gateway/src/user-rpc.service.ts
+++ examples/v0.6.0-rpc-gateway/src/user-rpc.service.ts
@@ -4,3 +4,7 @@
  * 没有装饰器、没有继承——`registerService()` 直接读原型上的方法名。
  * 这样业务代码对框架零依赖，单测时可以当普通对象直接调。
  */
+import { RpcError } from '@nofault/rpc';
+
+/** 业务错误码：4xx 段留给调用方看得懂的错误，网关据此翻译成 HTTP 状态 */
+export const USER_NOT_FOUND = 404;
