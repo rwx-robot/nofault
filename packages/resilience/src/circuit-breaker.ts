7db5e66bc3a50c2dd751106596b2f8d3efbf09ae
Ny <johnnynode@gmail.com>
2022-03-08 22:47:25 +0000

    feat(resilience): harden stats snapshot

--- packages/resilience/src/circuit-breaker.ts
+++ packages/resilience/src/circuit-breaker.ts
@@ -52,3 +52,4 @@ export class CircuitBreaker {
 
   constructor(private readonly options: CircuitBreakerOptions) {
     this.halfOpenMaxCalls = options.halfOpenMaxCalls ?? 1;
+    this.now = options.now ?? Date.now;
