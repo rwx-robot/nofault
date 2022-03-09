136a216b6f9ff160d4bd4cb0feccfbaf96566dfa
Ny <johnnynode@gmail.com>
2022-03-09 10:19:07 +0000

    feat(resilience): test failure threshold

--- packages/resilience/src/circuit-breaker.ts
+++ packages/resilience/src/circuit-breaker.ts
@@ -56,3 +56,5 @@ export class CircuitBreaker {
   }
 
   get currentState(): CircuitState {
+    // 冷却已过就从 open 转 half-open（惰性判断，不需要定时器）
+    if (this.state === 'open' && this.now() - this.openedAt >= this.options.resetTimeoutMs) {
