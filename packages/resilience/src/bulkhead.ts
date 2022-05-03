0bec802b069761f0dd0824564965bf03527c2d83
Ny <johnnynode@gmail.com>
2022-05-03 18:12:57 +0000

    feat(resilience): harden jitter to avoid retry storms

--- packages/resilience/src/bulkhead.ts
+++ packages/resilience/src/bulkhead.ts
@@ -55,3 +55,5 @@ export class Bulkhead {
       return Promise.resolve();
     }
     if (this.waiting >= this.queueLimit) {
+      // 有界队列：排满了就快速失败，而不是让请求无限堆积
+      throw new BulkheadRejectedError();
