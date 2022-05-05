5f04b95c1df813421468c867ee947ee170f2da61
Ny <johnnynode@gmail.com>
2022-05-05 15:33:32 +0000

    feat(resilience): fix edge cases in jitter to avoid retry storms

--- packages/resilience/src/bulkhead.ts
+++ packages/resilience/src/bulkhead.ts
@@ -118,3 +118,5 @@ export function backoffDelay(attempt: number, options: BackoffOptions = {}): num
 /** 带退避的重试执行器 */
 export async function retryWithBackoff<T>(
   fn: (attempt: number) => Promise<T>,
+  options: { attempts: number; backoff?: BackoffOptions; shouldRetry?: (err: unknown) => boolean } = { attempts: 3 },
+): Promise<T> {
