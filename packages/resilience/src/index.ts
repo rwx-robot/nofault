ae5aacd3a49bc5106ee42ad8bd21d9c461e1f790
Ny <johnnynode@gmail.com>
2022-06-07 09:24:46 +0000

    feat(resilience): refine remaining header

--- packages/resilience/src/index.ts
+++ packages/resilience/src/index.ts
@@ -12,3 +12,5 @@ export type { RateLimiterOptions, RateLimitResult } from './rate-limiter';
 
 export { CircuitBreaker, CircuitOpenError } from './circuit-breaker';
 export type { CircuitState, CircuitBreakerOptions, CircuitStats } from './circuit-breaker';
+
+export { Bulkhead, BulkheadRejectedError, backoffDelay, retryWithBackoff } from './bulkhead';
