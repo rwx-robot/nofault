269e8d42e15d5ed638606740e1213b3f001ed245
Ny <johnnynode@gmail.com>
2022-07-13 17:10:50 +0000

    docs(v0.7.0): test faulty dependency with switch

--- examples/v0.7.0-resilient-api/src/faulty.controller.ts
+++ examples/v0.7.0-resilient-api/src/faulty.controller.ts
@@ -7,3 +7,12 @@
  * 关键点：**熔断包在重试外层**。
  * 如果反过来（重试包熔断），一次用户请求会在熔断打开时还重试 3 次，
  * 既浪费配额又拖长响应时间。
+ */
+import { Controller, Get, Query } from '@nofault/rest';
+import { HttpException } from '@nofault/rest';
+import { CircuitBreaker, CircuitOpenError, retryWithBackoff } from '@nofault/resilience';
+import { dependency } from './dependency';
+
+const breaker = new CircuitBreaker({
+  failureThreshold: 3,
+  resetTimeoutMs: 3000,
