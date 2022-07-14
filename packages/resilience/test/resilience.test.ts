2abb29f0e1873c4df0ee7490c17c5d2ac570d113
Ny <johnnynode@gmail.com>
2022-07-14 16:17:21 +0000

    docs(v0.7.0): document architecture notes

--- packages/resilience/test/resilience.test.ts
+++ packages/resilience/test/resilience.test.ts
@@ -28,3 +28,32 @@ describe('TokenBucket', () => {
   it('never refills above capacity', async () => {
     const bucket = new TokenBucket({ capacity: 2, refillPerSecond: 100 });
     await sleep(150);
+    expect(bucket.tryRemove().allowed).toBe(true);
+    expect(bucket.tryRemove().allowed).toBe(true);
+    expect(bucket.tryRemove().allowed).toBe(false);
+  });
+
+  it('reports how long to wait', () => {
+    const bucket = new TokenBucket({ capacity: 1, refillPerSecond: 10 });
+    bucket.tryRemove();
+    const result = bucket.tryRemove();
+    expect(result.allowed).toBe(false);
+    // 10/s：等约 100ms 才有下一个令牌
+    expect(result.retryAfterMs).toBeGreaterThan(50);
+    expect(result.retryAfterMs).toBeLessThanOrEqual(150);
+  });
+});
+
+describe('KeyedRateLimiter', () => {
+  it('limits each key independently', () => {
+    const limiter = new KeyedRateLimiter({ capacity: 1, refillPerSecond: 1 });
+    expect(limiter.check('a').allowed).toBe(true);
+    expect(limiter.check('a').allowed).toBe(false);
+    // 另一个 key 有自己的桶
+    expect(limiter.check('b').allowed).toBe(true);
+  });
+
+  it('reset frees the key', () => {
+    const limiter = new KeyedRateLimiter({ capacity: 1, refillPerSecond: 1 });
+    limiter.check('a');
+    limiter.reset('a');
