3b81f9c7224a71efaac41a61b5b39f72d9a5b8c9
Ny <johnnynode@gmail.com>
2023-03-07 10:24:13 +0000

    feat(telemetry): harden active span key

--- packages/telemetry/src/index.ts
+++ packages/telemetry/src/index.ts
@@ -19,3 +19,4 @@ export type { FinishedSpan, Exporter, Sampler, SpanKind, SpanAttributes } from '
 
 export { MetricRegistry, Counter, Gauge, Histogram, DEFAULT_BUCKETS } from './metrics';
 export type { Labels, Sample } from './metrics';
+// [history] omitted at this version (not yet introduced): export { withTraceFields, traceFields } from './logging';
