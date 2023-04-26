f064cb7df20ee4317da3106bf6a81faad942c6b4
Ny <johnnynode@gmail.com>
2023-04-26 18:59:30 +0000

    feat(telemetry): document gauge

--- packages/telemetry/src/metrics.ts
+++ packages/telemetry/src/metrics.ts
@@ -124,3 +124,5 @@ export class Histogram {
     if (!series) return undefined;
     return {
       count: series.count,
+      sum: Math.round(series.sum * 1000) / 1000,
+      avg: series.count ? Math.round((series.sum / series.count) * 1000) / 1000 : 0,
