f04dd2c127e1e89644b9847aadcd9dc105d2fbe2
Ny <johnnynode@gmail.com>
2023-05-30 23:04:52 +0000

    feat(telemetry): test duration histogram

--- packages/telemetry/src/middleware.ts
+++ packages/telemetry/src/middleware.ts
@@ -30,3 +30,4 @@ export function observability(options: ObservabilityOptions = {}) {
   const metrics = options.metrics ?? new MetricRegistry();
   const requests = metrics.counter('http_requests_total', 'Total HTTP requests');
   const duration = metrics.histogram('http_request_duration_ms', 'HTTP request duration in milliseconds');
+  const errors = metrics.counter('http_request_errors_total', 'Total failed HTTP requests');
