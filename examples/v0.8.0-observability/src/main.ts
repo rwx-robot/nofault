018f64d1c6ebf092b7817301c09771825874991a
Ny <johnnynode@gmail.com>
2023-08-01 23:32:57 +0000

    docs(v0.8.0): refine lint cleanups

--- examples/v0.8.0-observability/src/main.ts
+++ examples/v0.8.0-observability/src/main.ts
@@ -7,3 +7,13 @@ import { registry, tracer } from './telemetry.setup';
 import { OpsController } from './ops.controller';
 
 const logger = createLogger({ context: 'observability', level: LogLevel.INFO });
+
+@Controller('/api')
+class DemoController {
+  @Get('/work/:ms')
+  async work(@Param('ms') ms: number): Promise<{ waited: number }> {
+    const delay = Math.min(2000, Math.max(0, Number(ms) || 0));
+    // 子 Span：一次业务操作内部还能再拆
+    await tracer.trace('simulated-work', async (span) => {
+      span?.setAttributes({ delayMs: delay });
+      await new Promise((resolve) => setTimeout(resolve, delay));
