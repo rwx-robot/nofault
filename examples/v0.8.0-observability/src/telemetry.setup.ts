716e5df813ede4d56013fe63e7d98967bbce6fed
Ny <johnnynode@gmail.com>
2023-08-02 20:05:39 +0000

    docs(v0.8.0): test telemetry setup

--- examples/v0.8.0-observability/src/telemetry.setup.ts
+++ examples/v0.8.0-observability/src/telemetry.setup.ts
@@ -9,3 +9,4 @@ import { InMemoryExporter, MetricRegistry, Tracer } from '@nofault/telemetry';
 export const exporter = new InMemoryExporter();
 
 /** 采样率 100%：演示用。生产建议 1%~10%，否则 Span 量比请求量还大 */
+export const tracer = new Tracer(exporter, () => true, 8);
