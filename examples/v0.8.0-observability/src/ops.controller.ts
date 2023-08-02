a6190455de0c33853822823bf160ffebac8cde83
Ny <johnnynode@gmail.com>
2023-08-02 12:50:06 +0000

    docs(v0.8.0): harden example readme

--- examples/v0.8.0-observability/src/ops.controller.ts
+++ examples/v0.8.0-observability/src/ops.controller.ts
@@ -3,3 +3,6 @@
  *
  * 这两个端点**不参与业务指标统计**：否则"看监控"这个动作本身
  * 会污染监控数据（采集频率一高就全是它自己的记录）。
+ */
+import { Controller, Get, Ctx, type RestContext } from '@nofault/rest';
+import { exporter, registry, tracer } from './telemetry.setup';
