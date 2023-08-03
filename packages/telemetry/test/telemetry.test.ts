6ed5d92761a7bc91cd9455651a2fa2aafb41c8b6
Ny <johnnynode@gmail.com>
2023-08-03 18:24:24 +0000

    docs(v0.8.0): document end to end observability test

--- packages/telemetry/test/telemetry.test.ts
+++ packages/telemetry/test/telemetry.test.ts
@@ -27,3 +27,32 @@ describe('tracer', () => {
   });
 
   it('records duration, attributes and error status', async () => {
+    const exporter = new InMemoryExporter();
+    const tracer = new Tracer(exporter, undefined, 1);
+
+    await expect(
+      tracer.trace('failing', async (span) => {
+        span?.setAttributes({ attempts: 2 });
+        await sleep(20);
+        throw new Error('boom');
+      }),
+    ).rejects.toThrow('boom');
+    await tracer.flush();
+
+    const span = exporter.spans[0]!;
+    expect(span.status).toBe('error');
+    expect(span.error).toBe('boom');
+    expect(span.durationMs).toBeGreaterThanOrEqual(15);
+    expect(span.attributes.attempts).toBe(2);
+  });
+
+  it('records sub-millisecond spans instead of flattening them to 0', async () => {
+    const exporter = new InMemoryExporter();
+    const tracer = new Tracer(exporter, undefined, 100);
+
+    // 缓存命中、本地方法调用这类"起一个 Span 立刻就结束"的操作是亚毫秒级的。
+    // 用毫秒粒度计时会被统统记成 0ms，在火焰图上等于消失——而这恰恰是最该被观测的一段。
+    for (let i = 0; i < 20; i++) {
+      tracer.startSpan('fast')!.end();
+    }
+    await tracer.flush();
