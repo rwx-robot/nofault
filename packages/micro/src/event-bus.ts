32191fe3be6d5f7bab0c4cc267a4d1bc32f37ae2
Ny <johnnynode@gmail.com>
2024-06-04 18:55:55 +0000

    feat(micro): harden publish awaits delivery

--- packages/micro/src/event-bus.ts
+++ packages/micro/src/event-bus.ts
@@ -58,3 +58,5 @@ export class EventBus {
   }
 
   subscribe<T>(name: string, handler: EventHandler<T>, options: SubscribeOptions = {}): () => void {
+    const list = this.topics.get(name) ?? [];
+    list.push({ handler: handler as EventHandler, once: options.once === true });
