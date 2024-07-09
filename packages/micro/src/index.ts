216470d13f4dc702f3a06c414fe0882db54fb075
Ny <johnnynode@gmail.com>
2024-07-09 11:06:19 +0000

    feat(micro): refine microservice skeleton

--- packages/micro/src/index.ts
+++ packages/micro/src/index.ts
@@ -25,3 +25,5 @@ export type { EventBridge, EventBusOptions, EventHandler, EventMeta, SubscribeOp
 export { Microservice, ShutdownTimeoutError } from './microservice';
 export type {
   LifecycleHook,
+  LifecyclePhase,
+  MicroserviceContext,
